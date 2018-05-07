140bfc0e3cb4847a35abc6550621573310ff235f
hkurra <er.harshkurra@gmail.com>
2018-05-07 06:53:06 +0530

    Fixed App loading error on iOS : changed js bundle name in app delegate

---
+++ app/stores/TreecounterStore.native.js
@@ -0,0 +1,33 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import reducers from '../reducers/reducer';
+import { initialState as entitiesState } from '../reducers/entitiesReducer';
+import { drawerNavMiddleware } from '../helpers/reduxHelpers.native';
+/**
+ * This function will be called in App.js by either:
+ *   1. ReactOnRails.registerStore({ TreecounterStore: configureStore }) and receive props and context created by Symfony
+ *   2. a direct call: configureStore(initialProps, context) passing props and context from a config file
+ * The fact whether the context contains the key 'serverSide' can be used to determine if the app is being initialized
+ * via symfony (1. 'serverSide' is defined) or from a local index.html file (2. 'serverSide' is not defined).
+ *
+ * @see app/config/index.js for a description of the parameters
+ *
+ * @param props initial data from Symfony or config
+ * @param context server environment provided by either Symfony or config
+ * @returns {Store}
+ */
+export default function configureStore(props, context) {
+  const initialState = {};
+
+  // use devtools if we are in a browser and the extension is enabled
+  const composeEnhancers =
+    typeof window !== 'undefined' &&
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose);
+
+  return createStore(
+    reducers,
+    initialState,
+    composeEnhancers(applyMiddleware(thunkMiddleware, drawerNavMiddleware))
+  );
+}

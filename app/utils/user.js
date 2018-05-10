8c531a10832b608c6f99a3c0a8cb1195560af835
niteshgrg <niteshgarg.06@gmail.com>
2018-05-10 14:33:42 +0530

    Test prehook

--- app/utils/user.js
+++ app/utils/user.js
@@ -1,11 +1,10 @@
-import { fetchItem } from '../stores/localStorage';
+import { fetchItem } from "../stores/localStorage";
 
 export function getAccessToken() {
-  let token = fetchItem('jwt');
+  let token = fetchItem("jwt");
 
   if (!token) {
     return null;
   }
-
   return token;
 }

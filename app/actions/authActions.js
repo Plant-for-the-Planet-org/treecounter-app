13e664a3091ac90558547a42f6aae5d5ba0f6158
hkurra <er.harshkurra@gmail.com>
2018-05-11 00:23:53 +0530

    Enabled pop up notification for web

--- app/actions/authActions.js
+++ app/actions/authActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
-//import { NotificationManager } from 'react-notifications';
+import { NotificationManager } from '../notification/PopupNotificaiton/notificationManager';
 
 //import { history } from '../components/Common/BrowserRouter';
 import { setUserLogIn, setUserLogOut } from '../reducers/authenticationReducer';
@@ -28,7 +28,7 @@ export function login(data) {
         };
         dispatch(setUserLogIn(payload));
 
-        // NotificationManager.success('Login Successful', 'Welcome', 5000);
+        NotificationManager.success('Login Successful', 'Welcome', 5000);
         // history.push({
         //   pathname: getLocalRoute('app_userHome'),
         //   state: { id: res.data.data.id }
@@ -48,13 +48,13 @@ export function login(data) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
           debug(error.response.data);
-          // NotificationManager.error(
-          //   error.response.data.message,
-          //   error.response.data.code,
-          //   5000
-          // );
+          NotificationManager.error(
+            error.response.data.message,
+            error.response.data.code,
+            5000
+          );
         } else {
-          //NotificationManager.error(error.message, 'Login Error', 5000);
+          NotificationManager.error(error.message, 'Login Error', 5000);
         }
       });
   };
@@ -107,9 +107,9 @@ export function forgot_password(data) {
     .post(getApiRoute('auth_forgotPassword_post'), data)
     .then(res => {
       debug(res.status);
-      // NotificationManager.success(
-      //   'Further details have been sent to your mail address'
-      // );
+      NotificationManager.success(
+        'Further details have been sent to your mail address'
+      );
       // history.push(getLocalRoute('app_login'));
     })
     .catch(err => debug(err));
---
+++ app/notification/PopupNotificaiton/notificationManager.native.js
@@ -0,0 +1,9 @@
+export default class NotificationManager {
+  sucess(message, title, timeOut) {
+    //TODO hkurra implement this using native snack bar on platform
+  }
+
+  error(message, title, timeOut) {
+    //TODO hkurra implement this using native snack bar on platform
+  }
+}

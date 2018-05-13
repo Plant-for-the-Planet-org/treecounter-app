27adc0b0cf4252eefc45da5d0284490d9b889ab0
niteshgrg <niteshgarg.06@gmail.com>
2018-05-13 15:47:00 +0530

    Add dummy loclazation setup

--- app/components/TreecounterGraphics/Trillion.native.js
+++ app/components/TreecounterGraphics/Trillion.native.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import i18n from '../../locales/i18n';
 
 export default class Trillion extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native with react navigation navigation{' '}
+          Welcome to React Native with react navigation navigation{' ' +
+            i18n.t('name.label')}
         </Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
       </View>
---
+++ app/locales/i18n.js
@@ -0,0 +1,28 @@
+import i18next from 'i18next';
+
+i18next.init({
+  interpolation: {
+    // React already does escaping
+    escapeValue: false
+  },
+  lng: 'en', // 'en' | 'es'
+  // Using simple hardcoded resources for simple example
+  resources: {
+    en: {
+      translation: {
+        age: { label: 'Age' },
+        home: { label: 'Home' },
+        name: { label: 'Name' }
+      }
+    },
+    es: {
+      translation: {
+        age: { label: 'Años' },
+        home: { label: 'Casa' },
+        name: { label: 'Nombre' }
+      }
+    }
+  }
+});
+
+export default i18next;

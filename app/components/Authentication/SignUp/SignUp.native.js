fe60162003ab8745a246d61281420ce6db7ce019
niteshgrg <niteshgarg.06@gmail.com>
2018-05-16 21:53:23 +0530

    Add nested field structure to parser

--- app/components/Authentication/SignUp/SignUp.native.js
+++ app/components/Authentication/SignUp/SignUp.native.js
@@ -14,6 +14,7 @@ import {
   schemaOptions,
   signupFormSchema
 } from '../../../server/formSchemas/signup';
+
 import { loginStyles } from '../Login.native';
 import SignupTypes from './SignupType.native';
 
--- app/server/parserLiformToTcomb.js
+++ app/server/parserLiformToTcomb.js
@@ -3,12 +3,8 @@ let transform = require('tcomb-json-schema');
 import { TextInputTemplate } from '../components/Common/Templates/TextInputTemplate';
 export default function parseJsonToTcomb(liformSchemaJson) {
   let liformSchema = JSON.parse(JSON.stringify(liformSchemaJson));
-  let schemaOptions = {
-    fields: {}
-  };
 
   let properties = liformSchema.properties;
-  let fields = schemaOptions.fields;
 
   let newEnum = {};
   for (let propertyKey in properties) {
@@ -24,27 +20,41 @@ export default function parseJsonToTcomb(liformSchemaJson) {
     }
   }
 
-  for (let propertyKey in properties) {
-    if (properties.hasOwnProperty(propertyKey)) {
-      let options = {};
-      options.placeholder = properties[propertyKey].title;
-      options.autoCapitalize = 'none';
-      if (
-        properties[propertyKey].type &&
-        properties[propertyKey].type === 'string'
-      ) {
-        if (!properties[propertyKey].hasOwnProperty('enum')) {
-          options.template = TextInputTemplate;
+  function getSchemaOptions(properties) {
+    let schemaOptions = {
+      fields: {}
+    };
+    for (let propertyKey in properties) {
+      if (properties.hasOwnProperty(propertyKey)) {
+        let options = {};
+        options.placeholder = properties[propertyKey].title;
+        options.autoCapitalize = 'none';
+        if (
+          properties[propertyKey].type &&
+          properties[propertyKey].type === 'string'
+        ) {
+          if (!properties[propertyKey].hasOwnProperty('enum')) {
+            options.template = TextInputTemplate;
+          } else {
+            options.label = '';
+          }
+        }
+        if (properties[propertyKey].widget === 'password') {
+          options.secureTextEntry = true;
+        }
+        if (properties[propertyKey].type === 'object') {
+          schemaOptions['fields'][propertyKey] = getSchemaOptions(
+            properties[propertyKey].properties
+          );
         } else {
-          options.label = '';
+          schemaOptions['fields'][propertyKey] = options;
         }
       }
-      if (properties[propertyKey].widget === 'password') {
-        options.secureTextEntry = true;
-      }
-      fields[propertyKey] = options;
     }
+    return schemaOptions;
   }
+
+  let schemaOptions = getSchemaOptions(properties);
 
   let transformedSchema = transform(liformSchema);
   return { schemaOptions: schemaOptions, transformedSchema: transformedSchema };

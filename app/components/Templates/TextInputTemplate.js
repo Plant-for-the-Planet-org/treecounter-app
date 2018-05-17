12b76b1062891cae367d4327339045787138620e
Karan Hudia <karanhudia@gmail.com>
2018-05-17 18:27:47 +0530

    Added material textfield template

--- app/components/Templates/TextInputTemplate.js
+++ app/components/Templates/TextInputTemplate.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 export function TextInputTemplate(locals) {
   return (
-    <div>
-      {locals.config.iconUrl ? <img src={locals.config.iconUrl} /> : null}
-      <input placeholder={locals.placeholder} />
+    <div className="pftp-textfield">
+      {locals.config.iconUrl ? (
+        <img className="pftp-textfield__icon" src={locals.config.iconUrl} />
+      ) : null}
+      <div className="pftp-textfield__inputgroup">
+        <input type="text" required="required" />
+        <span className="pftp-textfield__inputgroup--highlight" />
+        <span className="pftp-textfield__inputgroup--bar" />
+        <label>{locals.placeholder}</label>
+      </div>
     </div>
   );
 }

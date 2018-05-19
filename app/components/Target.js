982c036e2fbbb652935f1c7bef74a5f15539a182
niteshgrg <niteshgarg.06@gmail.com>
2018-05-20 02:55:57 +0530

    Remove api call and add initial values from store

--- app/components/Target.js
+++ app/components/Target.js
@@ -4,48 +4,29 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 
-import * as Schema from '../layouts/TargetSchema';
+import targetFormSchema from '../server/formSchemas/target';
 import CustomForm from './Common/CustomForm';
-import LoadingIndicator from './Common/LoadingIndicator';
 import { SubmitTarget } from '../actions/targetAction';
 import { userTreecounterSelector } from '../selectors/index';
 
 class TargetPage extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
+    let { treecounter } = this.props;
+    let { count_target, target_year, target_comment } = treecounter;
     this.state = {
-      data: {},
-      loading: true,
-      label: 'Set',
-      schema: {},
-      initialValues: {}
+      label: treecounter.countTarget !== 0 ? 'Update' : 'Set',
+      initialValues: { count_target, target_year, target_comment }
     };
-  }
-
-  componentDidMount() {
-    const { treecounter } = this.props;
-    if (null !== treecounter) {
-      Schema.TargetSchema(this.props.treecounter.id).subscribe(success => {
-        this.setState({
-          schema: success.schema,
-          loading: false,
-          initialValues: success.values,
-          label: success.values.countTarget !== 0 ? 'Update' : 'Set'
-        });
-      });
-    }
   }
 
   render() {
     const { treecounter } = this.props;
     return (
       <div>
-        <div className="text-center">
-          {this.state.loading ? <LoadingIndicator /> : null}
-        </div>
         <LiForm
-          schema={this.state.schema}
+          schema={targetFormSchema}
           initialValues={this.state.initialValues}
           onSubmit={treecounterData =>
             this.props.SubmitTarget(treecounterData, treecounter.id)

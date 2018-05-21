5c0259de79d3f91288082dd48efbbde766631798
niteshgrg <niteshgarg.06@gmail.com>
2018-05-21 18:47:36 +0530

    Save menu offline

--- app/containers/Menu/SideMenuContainer.js
+++ app/containers/Menu/SideMenuContainer.js
@@ -2,48 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+
 import Menu from '../../components/Menu/index';
+import sideMenuData from '../../server/sidemenu';
 
 // Actions
-import * as Sidebar from '../../actions/menuAction';
 import { logoutUser } from '../../actions/authActions';
 
 class SideMenuContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    };
-  }
-
-  componentDidMount() {
-    this.fetchAndSetMenuState();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.loggedIn !== this.props.loggedIn && this.props.loggedIn)
-      this.fetchAndSetMenuState();
-  }
-
-  fetchAndSetMenuState() {
-    Sidebar.MenuAction(this.props.loggedIn)
-      .then(({ data }) => {
-        this.setState({ data: data });
-        console.log('Recieved data for menu- ', data);
-      })
-      .catch(error => {
-        if (error.response.status === 401) {
-          console.log('OOPS! JWT Token Expired');
-          this.props.logoutUser();
-        }
-      });
-  }
   render() {
-    console.log('Menu object', this.state.data);
+    let menuData = this.props.loggedIn
+      ? sideMenuData.loggedIn
+      : sideMenuData.loggedOut;
     return (
       <Menu
         isOpen={this.props.isOpen}
-        menuData={this.state.data}
+        menuData={menuData}
         navigation={this.props.navigation}
       />
     );
---
+++ app/server/sidemenu.js
@@ -0,0 +1,188 @@
+export default {
+  loggedOut: [
+    {
+      enabled: 1,
+      sequence: 1,
+      caption: 'World',
+      help: 'menu_group.world.help',
+      icon: 'none',
+      menuItems: [
+        {
+          enabled: 1,
+          sequence: 1,
+          caption: 'Trillion Tree Campaign',
+          help: 'menu_item.trillion_tree_campaign.help',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en'
+        },
+        {
+          enabled: 1,
+          sequence: 2,
+          caption: 'Explore',
+          help: 'menu_item.explore.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        }
+      ]
+    },
+    {
+      enabled: 1,
+      sequence: 2,
+      caption: 'My Items',
+      help: 'menu_group.user.help',
+      icon: 'none',
+      menuItems: [
+        {
+          enabled: 1,
+          sequence: 1,
+          caption: 'Register planted trees',
+          help: 'menu_item.register_trees.help',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en/registerTrees'
+        },
+        {
+          enabled: 1,
+          sequence: 2,
+          caption: 'Donate Trees',
+          help: 'menu_item.donate_trees.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        },
+        {
+          enabled: 1,
+          sequence: 3,
+          caption: 'My Trees',
+          help: 'menu_item.my_trees.help',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en/myTrees'
+        },
+        {
+          enabled: 1,
+          sequence: 4,
+          caption: 'Set Target',
+          help: 'Set your personal target',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en/target'
+        }
+      ]
+    },
+    {
+      enabled: 1,
+      sequence: 3,
+      caption: 'Community',
+      help: 'menu_group.community.help',
+      icon: 'none',
+      menuItems: [
+        {
+          enabled: 1,
+          sequence: 1,
+          caption: 'Challenge',
+          help: 'menu_item.challenge.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        },
+        {
+          enabled: 1,
+          sequence: 2,
+          caption: 'Tree gift',
+          help: 'menu_item.tree_gift.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        }
+      ]
+    }
+  ],
+  loggedIn: [
+    {
+      enabled: 1,
+      sequence: 1,
+      caption: 'World',
+      help: 'menu_group.world.help',
+      icon: 'none',
+      menuItems: [
+        {
+          enabled: 1,
+          sequence: 1,
+          caption: 'Trillion Tree Campaign',
+          help: 'menu_item.trillion_tree_campaign.help',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en'
+        },
+        {
+          enabled: 1,
+          sequence: 2,
+          caption: 'Explore',
+          help: 'menu_item.explore.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        }
+      ]
+    },
+    {
+      enabled: 1,
+      sequence: 2,
+      caption: 'My Items',
+      help: 'menu_group.user.help',
+      icon: 'none',
+      menuItems: [
+        {
+          enabled: 1,
+          sequence: 1,
+          caption: 'Register planted trees',
+          help: 'menu_item.register_trees.help',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en/registerTrees'
+        },
+        {
+          enabled: 1,
+          sequence: 2,
+          caption: 'Donate Trees',
+          help: 'menu_item.donate_trees.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        },
+        {
+          enabled: 1,
+          sequence: 3,
+          caption: 'My Trees',
+          help: 'menu_item.my_trees.help',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en/myTrees'
+        },
+        {
+          enabled: 1,
+          sequence: 4,
+          caption: 'Set Target',
+          help: 'Set your personal target',
+          icon: 'some_icon_name',
+          uri: '/app_dev.php/en/target'
+        }
+      ]
+    },
+    {
+      enabled: 1,
+      sequence: 3,
+      caption: 'Community',
+      help: 'menu_group.community.help',
+      icon: 'none',
+      menuItems: [
+        {
+          enabled: 1,
+          sequence: 1,
+          caption: 'Challenge',
+          help: 'menu_item.challenge.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        },
+        {
+          enabled: 1,
+          sequence: 2,
+          caption: 'Tree gift',
+          help: 'menu_item.tree_gift.help',
+          icon: 'some_icon_name',
+          uri: '#'
+        }
+      ]
+    }
+  ]
+};

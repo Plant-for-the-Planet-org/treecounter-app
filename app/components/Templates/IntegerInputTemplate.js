93314d488561dd10e80660885edfbe9793a82826
Karan Hudia <karanhudia@gmail.com>
2018-05-22 10:26:37 +0530

    Fixed integer template to be based on patterns onchange

--- app/components/Templates/IntegerInputTemplate.js
+++ app/components/Templates/IntegerInputTemplate.js
@@ -3,22 +3,24 @@ import i18n from '../../locales/i18n';
 
 export function IntegerInputTemplate(locals) {
   function onChange($event) {
-    locals.onChange($event.target.value);
+    if (!isNaN($event.target.value)) locals.onChange($event.target.value);
   }
   return (
-    <div className="pftp-intfield">
+    <div className="pftp-textfield">
       {locals.config.iconUrl ? (
-        <img className="pftp-intfield__icon" src={locals.config.iconUrl} />
+        <img className="pftp-textfield__icon" src={locals.config.iconUrl} />
       ) : null}
-      <div className="pftp-intfield__inputgroup">
+      <div className="pftp-textfield__inputgroup">
         <input
-          type="number"
+          type={locals.type}
           autoComplete="new-password"
           required="required"
           value={locals.value}
           onChange={onChange}
-          placeholder={i18n.t(locals.label)}
         />
+        <span className="pftp-textfield__inputgroup--highlight" />
+        <span className="pftp-textfield__inputgroup--bar" />
+        <label>{i18n.t(locals.label)}</label>
       </div>
     </div>
   );

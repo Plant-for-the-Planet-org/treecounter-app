e17126e1a139292ec09ed4bc1b60b548c1bcc18e
Karan Hudia <karanhudia@gmail.com>
2018-05-29 19:50:19 +0530

    Implemented faq container and component without proper UI

--- app/components/FAQ/index.js
+++ app/components/FAQ/index.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
+import renderHTML from 'react-render-html';
 
 export default class FAQ extends Component {
+  GetFAQList() {
+    return this.props.faqs.map(faq => (
+      <AccordionItem>
+        <AccordionItemTitle>
+          <h3 className="u-position-relative">
+            {faq.question}
+            <div className="accordion__arrow" role="presentation" />
+          </h3>
+        </AccordionItemTitle>
+        <AccordionItemBody>{renderHTML(faq.answer)}</AccordionItemBody>
+      </AccordionItem>
+    ));
+  }
   render() {
-    return <div className="app-container__content--center">Some FAQ</div>;
+    return (
+      <div className="app-container__content--center">
+        <Accordion>{this.GetFAQList()}</Accordion>
+      </div>
+    );
   }
 }
 
-FAQ.propTypes = {};
+FAQ.propTypes = {
+  faqs: PropTypes.array.isRequired
+};
--- app/containers/FAQ/index.js
+++ app/containers/FAQ/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 
 import FAQ from '../../components/FAQ';
+import { FAQAction } from '../../actions/faqAction';
+import LoadingIndicator from '../../components/Common/LoadingIndicator';
 
 class FAQContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      faqs: []
+    };
+  }
+
+  componentWillMount() {
+    FAQAction().then(
+      success => {
+        this.setState({
+          loading: false,
+          faqs: success.data
+        });
+      },
+      error => console.log(error)
+    );
+  }
+
   render() {
-    return <FAQ />;
+    return this.state.loading ? (
+      <LoadingIndicator />
+    ) : (
+      <FAQ faqs={this.state.faqs} />
+    );
   }
 }
 
 export default FAQContainer;
-
-FAQContainer.propTypes = {};

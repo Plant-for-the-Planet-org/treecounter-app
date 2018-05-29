42cd96525b2a694cdeec3a354e9163684c9d5e66
Karan Hudia <karanhudia@gmail.com>
2018-05-29 19:12:48 +0530

    Basic faq container and component structure

---
+++ app/components/FAQ/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+export default class FAQ extends Component {
+  render() {
+    return <div className="app-container__content--center">Some FAQ</div>;
+  }
+}
+
+FAQ.propTypes = {};
---
+++ app/containers/FAQ/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import FAQ from '../../components/FAQ';
+
+class FAQContainer extends React.Component {
+  render() {
+    return <FAQ />;
+  }
+}
+
+export default FAQContainer;
+
+FAQContainer.propTypes = {};

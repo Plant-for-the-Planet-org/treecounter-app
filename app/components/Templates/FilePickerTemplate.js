1dd8b612ad36db7ccdf5338aadb266c844b9b0d9
Karan Hudia <karanhudia@gmail.com>
2018-05-30 19:04:47 +0530

    Allow image upload and register tree flow

--- app/components/Templates/FilePickerTemplate.js
+++ app/components/Templates/FilePickerTemplate.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 export function FilePickerTemplate(locals) {
   function onChange($event) {
-    locals.onChange($event.target.value);
+    let value;
+    let fileReader = new FileReader();
+    fileReader.addEventListener('load', event => {
+      value = event.target.result;
+      locals.onChange(value);
+    });
+    if ($event.target.files && $event.target.files[0]) {
+      fileReader.readAsDataURL($event.target.files[0]);
+    } else locals.onChange($event.target.value);
   }
 
   return (

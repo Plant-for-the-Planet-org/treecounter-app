03655a16cb7cfc2940bca5da93efa79f28ee86fb
Jorgo Miridis <jorgo@miridis.com>
2018-06-04 15:24:36 -0500

    denormalize selectedPlantProject to gain access to tpo name

--- app/selectors/index.js
+++ app/selectors/index.js
@@ -101,12 +101,16 @@ export const sortedUserContributionsSelector = createSelector(
  */
 export const selectedPlantProjectSelector = createSelector(
   selectedPlantProjectIdSelector,
-  getPlantProjects,
-  (selectedPlantProjectId, plantProjects) => {
+  entitiesSelector,
+  (selectedPlantProjectId, entities) => {
     logSelectorUpdate('selectedPlantProjectSelector');
     return null === selectedPlantProjectId
       ? null
-      : plantProjects[selectedPlantProjectId];
+      : denormalize(
+          entities.plantProjects[selectedPlantProjectId],
+          userProfileSchema,
+          entities
+        );
   }
 );
 

241e189de80e15f75031c6b381a0517651290ef3
niteshgrg <niteshgarg.06@gmail.com>
2018-06-05 20:46:44 +0530

    Remove adaptive height

--- app/components/DonateTrees/DonateTress.js
+++ app/components/DonateTrees/DonateTress.js
@@ -60,7 +60,6 @@ export default class DonateTrees extends Component {
   render() {
     const settings = {
       dots: true,
-      adaptiveHeight: true,
       nextArrow: (
         <CarouselNavigation
           styleName="donate-tree-nav-img__right"

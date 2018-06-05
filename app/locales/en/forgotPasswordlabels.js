5bb8d81c4a6a15b491f7695fb72244982a5a3cf6
bansallakshi <bansallakshi97@gmail.com>
2018-06-05 10:08:51 +0530

    Added Loginlabels and forgotPasswordlabels

---
+++ app/locales/en/forgotPasswordlabels.js
@@ -0,0 +1,9 @@
+export default {
+  forgot_ur_password: 'Forgot your password?',
+  enter_mail:
+    'Enter your email address and we‘ll send you a link to reset your password.',
+  forgot_password: 'forgot_password',
+  email: 'Email',
+  reset_password: 'Reset Password',
+  again_login: 'Try to login again'
+};
---
+++ app/locales/en/loginlabels.js
@@ -0,0 +1,9 @@
+export default {
+  forgot_password: 'Forgot your password?&nbsp;',
+  reset: 'Reset.',
+  dont_have_account: "Don't have an account?&nbsp;",
+  signup: 'Sign up.',
+  login: 'Log In',
+  email: 'Email',
+  password: 'Password'
+};

b63cdc136908cbe4f138f410cdc318755a435b49
Jorgo Miridis <jorgo@miridis.com>
2018-06-06 13:09:05 -0500

    add tree count and currency selector

---
+++ app/components/Payment/PaymentSelector.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import PaymentOption from './PaymentOption';
+
+const PaymentSelector = ({
+  paymentMethods,
+  accounts,
+  currency,
+  amount,
+  onSuccess,
+  onFailure,
+  onError
+}) => {
+  console.log(
+    'paymentMethods, accounts, currency, amount',
+    paymentMethods,
+    accounts,
+    currency,
+    amount
+  );
+  console.log(
+    'PaymentSelector ==============================================================='
+  );
+
+  const options = [];
+  Object.keys(paymentMethods).map(gateway => {
+    const accountKey = paymentMethods[gateway];
+    const [accountName, target] = accountKey.split(':');
+
+    options.push({
+      gateway: gateway,
+      accountName: accountName,
+      currency,
+      amount,
+      target: target,
+      account: accounts[accountName],
+      selectable: true,
+      onSuccess,
+      onFailure,
+      onError
+    });
+  });
+  console.log(options);
+
+  return (
+    <div>
+      {options.map(option => (
+        <PaymentOption key={option.gateway} {...option} />
+      ))}
+    </div>
+  );
+};
+
+PaymentSelector.propTypes = {
+  paymentMethods: PropTypes.object.isRequired,
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+  accounts: PropTypes.object,
+  onSuccess: PropTypes.func.isRequired,
+  onFailure: PropTypes.func.isRequired,
+  onError: PropTypes.func.isRequired
+};
+
+export default PaymentSelector;

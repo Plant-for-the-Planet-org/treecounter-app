3497d4af2f5b64b38cee4f997e73aaefbafe3d70
Madhav <madhavgaba2@gmail.com>
2018-06-08 00:37:05 +0530

    added a check of user locale and dummy de file for testing

---
+++ app/locales/de/labels.js
@@ -0,0 +1,14 @@
+export default {
+  email: 'hello',
+  password: 'hello',
+  address: 'hello',
+  zipCode: 'hello',
+  city: 'hello',
+  country: 'hello',
+  password_repeat: 'hello',
+  mayPublish: 'hello',
+  mayContact: 'hello',
+  firstname: 'hello',
+  lastname: 'hello',
+  company_or_orga_name: 'hello'
+};

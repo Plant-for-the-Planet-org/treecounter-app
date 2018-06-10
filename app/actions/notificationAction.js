eba452a86be4526877bbf9988cae37628f7dc460
niteshgrg <niteshgarg.06@gmail.com>
2018-06-10 18:05:05 +0530

    Add sync more action

--- app/actions/notificationAction.js
+++ app/actions/notificationAction.js
@@ -1,5 +1,8 @@
 import { getAuthenticatedRequest } from '../utils/api';
-import { userFeedsSynced } from '../reducers/userFeedReducer';
+import {
+  userFeedsSynced,
+  userFeedsSyncedMore
+} from '../reducers/userFeedReducer';
 
 export function NotificationAction() {
   const request = getAuthenticatedRequest('userfeeds_get');
@@ -14,7 +17,7 @@ export function moreNotificationAction(id) {
   const request = getAuthenticatedRequest('userfeedsMore_get', { lastId: id });
   return dispatch => {
     request.then(res => {
-      dispatch(userFeedsSynced(res.data));
+      dispatch(userFeedsSyncedMore(res.data));
     });
   };
 }
--- app/reducers/userFeedReducer.js
+++ app/reducers/userFeedReducer.js
@@ -1,13 +1,22 @@
 import { createAction, handleActions } from 'redux-actions';
 
 export const userFeedsSynced = createAction('USER_FEED_SYNCED');
+export const userFeedsSyncedMore = createAction('USER_FEED_SYNCED_MORE');
+
 export const getUserFeeds = state => state.userFeeds;
 
 export const initialState = null;
 
 const userFeedReducer = handleActions(
   {
-    [userFeedsSynced]: (state, action) => action.payload
+    [userFeedsSynced]: (state, action) => action.payload,
+    [userFeedsSyncedMore]: (state, action) => {
+      let temp = state;
+      temp.userFeeds.concat(action.payload.userFeeds);
+      temp.more = action.payload.more;
+      temp.unRead = action.payload.unRead;
+      return temp;
+    }
   },
   initialState
 );

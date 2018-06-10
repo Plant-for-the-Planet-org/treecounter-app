24c1f1987d499aa0cc237e5886294dfce32bc739
hkurra <er.harshkurra@gmail.com>
2018-06-10 23:30:25 +0530

    Created new tComb's list template for Plant project form of edit user profile

--- app/server/parsedSchemas/editProfile.js
+++ app/server/parsedSchemas/editProfile.js
@@ -1,9 +1,6 @@
 import schemaLiform, { plantProject } from '../formSchemas/editProfile';
 import parseJsonToTcomb from '../parserLiformToTcomb';
-import { ListTemplateGenerator } from '../../components/Templates/ListTemplate';
-import React from 'react';
-import PrimaryButton from '../../components/Common/Button/PrimaryButton';
-import i18n from '../../locales/i18n.js';
+import PlantProjectListTemplate from '../../components/EditUserProfile/PlantProjectListTemplate';
 
 const parsedSchema = {};
 Object.keys(schemaLiform).map(userType => {
@@ -14,30 +11,9 @@ Object.keys(schemaLiform).map(userType => {
     }))
   );
 });
-console.log(parsedSchema);
-
-const listTemplateConfig = {
-  renderRowWithoutButtons: (row, locals) => {
-    console.log('renderRow', row, locals);
-    return (
-      <div className="plant-project__item">
-        <div className="row-header">Project </div>
-        {row.input}
-        <PrimaryButton
-          onClick={() => {
-            this.toggleConfirmProfileDeletion();
-          }}
-        >
-          {i18n.t('label.save_changes')}
-        </PrimaryButton>
-      </div>
-    );
-  }
-};
 
 const plantProjectSchema = parseJsonToTcomb(plantProject.schema, {
-  array: ListTemplateGenerator(listTemplateConfig)
+  array: PlantProjectListTemplate
 });
-console.log('plant project schema', plantProjectSchema, plantProject);
 
 export { parsedSchema, plantProjectSchema };

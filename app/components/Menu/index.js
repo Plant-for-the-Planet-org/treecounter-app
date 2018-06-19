34da2125a4911ce0e0412d8553d248052dfe784a
hkurra <harshkurra21@gmail.com>
2018-06-19 11:56:27 +0530

    Fixed compilation error

--- app/components/Menu/index.js
+++ app/components/Menu/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 // Images
 import * as images from '../../assets';

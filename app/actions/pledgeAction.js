8dd0ba4a7a53cb69f40a70b1207e96f3b713e038
niteshgrg <niteshgarg.06@gmail.com>
2018-06-20 02:55:26 +0530

    Api poll

--- app/actions/pledgeAction.js
+++ app/actions/pledgeAction.js
@@ -1,13 +1,17 @@
 import { getRequest } from '../utils/api';
-import { fetchPledges } from '../reducers/pledgeReducer';
+import { fetchPledges, saveTimeoutID } from '../reducers/pledgeReducer';
 
 export function fetchPledgesAction() {
-  const request = getRequest('pledgeEvent_get', {
-    eventSlug: 'esri-user-conference'
-  });
   return dispatch => {
-    request.then(res => {
-      dispatch(fetchPledges(res.data));
-    });
+    let timeoutID = setInterval(
+      () =>
+        getRequest('pledgeEvent_get', {
+          eventSlug: 'esri-user-conference'
+        }).then(res => {
+          dispatch(fetchPledges(res.data));
+        }),
+      10000
+    );
+    dispatch(saveTimeoutID(timeoutID));
   };
 }
--- app/reducers/pledgeReducer.js
+++ app/reducers/pledgeReducer.js
@@ -1,6 +1,7 @@
 import { createAction, handleActions } from 'redux-actions';
 
 export const fetchPledges = createAction('FETCH_PLEDGES');
+export const saveTimeoutID = createAction('SAVE_TIMEOUT_ID');
 
 export const getPledges = state => state.pledges;
 
@@ -8,7 +9,12 @@ export const initialState = null;
 
 const fetchPledgesReducer = handleActions(
   {
-    [fetchPledges]: (state, action) => action.payload
+    [fetchPledges]: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+    [saveTimeoutID]: (state, action) => {
+      return { ...state, timeoutID: action.payload };
+    }
   },
   initialState
 );

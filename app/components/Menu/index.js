fc14eb1cd62ffa8fe5126ba9c69fe7071ce55004
niteshgrg <niteshgarg.06@gmail.com>
2018-06-21 02:18:36 +0530

    Add menu icons

--- app/components/Menu/index.js
+++ app/components/Menu/index.js
@@ -46,7 +46,11 @@ export default class Menu extends Component {
                       <img
                         src={
                           menuItem.icon && menuItem.icon !== 'none'
-                            ? images[menuItem.icon]
+                            ? menuItem.uri.substr(
+                                menuItem.uri.lastIndexOf('/') + 1
+                              ) === path
+                              ? images[menuItem.icon + '_red']
+                              : images[menuItem.icon]
                             : null
                         }
                         className="menu-icon"

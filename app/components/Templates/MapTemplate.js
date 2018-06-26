7f7550056808627a8d39d6bb78ca727a734ed296
hkurra <er.harshkurra@gmail.com>
2018-06-27 01:57:36 +0530

    changes to load new map in form

--- app/components/Templates/MapTemplate.js
+++ app/components/Templates/MapTemplate.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import MapContributionCapture from '../Map/MapContributionCapture';
+import RegistrationMap from '../../components/RegisterTrees/RegistrationMap';
 
 export function MapTemplate(locals) {
-  console.log('%%%%%%%%%%%%%%%%%%%% '.locals);
+  console.log('%%%%%%%%%%%%%%%%%%%% ', locals);
   //  locals.onChange(); //Temporary fix
   return (
-    <MapContributionCapture
-      geoLocation={'geoLongitude=1&geoLatitude=1&country=DE'}
-      onLocationSelected={locals.onChange}
-    />
+    <RegistrationMap onGeoLocationSelected={locals.onChange} />
+    // <MapContributionCapture
+    //   geoLocation={{ geoLongitude: 1, geoLatitude: 1, country: 'DE' }}
+    //   onLocationSelected={locals.onChange}
+    // />
   );
 }

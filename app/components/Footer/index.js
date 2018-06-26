f3ccbd3c54926abd2bfcd154931256444ebd0237
hkurra <harshkurra21@gmail.com>
2018-06-26 17:21:28 +0530

    Made changes to have single footer image for both mobile and web

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -6,7 +6,6 @@ const Footer = () => {
   return (
     <div className="app-container__footer sidenav-wrapper">
       <img className="footer-logo" src={FooterLogos} />
-      <img className="footer-logo-mobile" src={FooterMobileLogos} />
     </div>
   );
 };

e6d377fac570d4c6c38ec96f146c4f74788aeeeb
niteshgrg <niteshgarg.06@gmail.com>
2018-06-28 01:33:49 +0530

    Add create action in reducer.native

--- app/reducers/reducer.native.js
+++ app/reducers/reducer.native.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
 
 import commonReducers from './commonReducers';
+import { createAction } from 'redux-actions';
 import { appDrawerReducer } from './appDrawerReducer';
 
-export default combineReducers({
+export const userLogout = createAction('USER_LOGOUT');
+
+const appReducer = combineReducers({
   ...commonReducers,
   appDrawer: appDrawerReducer
 });
+
+export default (state, action) => {
+  if (action.type === 'USER_LOGOUT') {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};

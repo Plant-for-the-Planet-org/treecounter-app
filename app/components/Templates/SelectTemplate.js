a641f6ff2cd1ed175a59cb0fe118c8f60ee137fe
hkurra <harshkurra21@gmail.com>
2018-06-29 19:31:31 +0530

    Added Required field and email validation with proper UI

--- app/components/Templates/SelectTemplate.js
+++ app/components/Templates/SelectTemplate.js
@@ -5,11 +5,16 @@ export function SelectTemplate(locals) {
   function onChange($event) {
     locals.onChange($event.target.value);
   }
+  console.log('test', locals);
   return (
     <div className="pftp-selectfield">
       <select
         key={locals.attrs.id}
-        className="pftp-selectfield__select"
+        className={
+          locals.hasError
+            ? 'pftp-selectfield__select-error'
+            : 'pftp-selectfield__select'
+        }
         required="required"
         onChange={onChange}
         value={locals.value}
--- app/components/Templates/TextInputTemplate.js
+++ app/components/Templates/TextInputTemplate.js
@@ -2,12 +2,6 @@ import React from 'react';
 import i18n from '../../locales/i18n';
 
 export function TextInputTemplate(locals) {
-  // let errorBlockStyle = locals.stylesheet.errorBlock;
-  let hasErr =
-    locals.hasError && locals.error ? (
-      <div accessibilityLiveRegion="polite">{locals.error}</div>
-    ) : null;
-
   function onChange($event) {
     let value =
       locals.type === 'number'
@@ -15,6 +9,7 @@ export function TextInputTemplate(locals) {
         : $event.target.value;
     locals.onChange(value);
   }
+
   return locals.type !== 'hidden' ? (
     <div>
       <div className="pftp-textfield">
@@ -35,12 +30,26 @@ export function TextInputTemplate(locals) {
             value={locals.value}
             onChange={onChange}
           />
-          <span className="pftp-textfield__inputgroup--highlight" />
-          <span className="pftp-textfield__inputgroup--bar" />
-          <label>{i18n.t(locals.label)}</label>
+          <span
+            className={
+              !locals.hasError
+                ? 'pftp-textfield__inputgroup--highlight'
+                : 'pftp-textfield__inputgroup--highlightr-error'
+            }
+            className=""
+          />
+          <span
+            className={
+              !locals.hasError
+                ? 'pftp-textfield__inputgroup--bar'
+                : 'pftp-textfield__inputgroup--error-bar'
+            }
+          />
+          <label className={locals.hasError ? 'error-label' : ''}>
+            {i18n.t(locals.label)}
+          </label>
         </div>
       </div>
-      {/* {Error} */}
     </div>
   ) : null;
 }

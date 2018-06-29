0f952f9549daae2d4a0bf94603a35ddabc343f2a
hkurra <er.harshkurra@gmail.com>
2018-06-29 09:26:59 +0530

    basic validation for text input

--- app/components/Templates/TextInputTemplate.js
+++ app/components/Templates/TextInputTemplate.js
@@ -2,6 +2,12 @@ import React from 'react';
 import i18n from '../../locales/i18n';
 
 export function TextInputTemplate(locals) {
+  // let errorBlockStyle = locals.stylesheet.errorBlock;
+  let hasErr =
+    locals.hasError && locals.error ? (
+      <div accessibilityLiveRegion="polite">{locals.error}</div>
+    ) : null;
+
   function onChange($event) {
     let value =
       locals.type === 'number'
@@ -10,22 +16,31 @@ export function TextInputTemplate(locals) {
     locals.onChange(value);
   }
   return locals.type !== 'hidden' ? (
-    <div className="pftp-textfield">
-      {locals.config.iconUrl ? (
-        <img className="pftp-textfield__icon" src={locals.config.iconUrl} />
-      ) : null}
-      <div className="pftp-textfield__inputgroup">
-        <input
-          type={locals.type}
-          autoComplete="new-password"
-          required="required"
-          value={locals.value}
-          onChange={onChange}
-        />
-        <span className="pftp-textfield__inputgroup--highlight" />
-        <span className="pftp-textfield__inputgroup--bar" />
-        <label>{i18n.t(locals.label)}</label>
+    <div>
+      <div className="pftp-textfield">
+        {locals.config.iconUrl ? (
+          <img className="pftp-textfield__icon" src={locals.config.iconUrl} />
+        ) : null}
+        <div
+          className={
+            !locals.hasError
+              ? 'pftp-textfield__inputgroup'
+              : 'pftp-textfield__error-inputgroup'
+          }
+        >
+          <input
+            type={locals.type}
+            autoComplete="new-password"
+            required="required"
+            value={locals.value}
+            onChange={onChange}
+          />
+          <span className="pftp-textfield__inputgroup--highlight" />
+          <span className="pftp-textfield__inputgroup--bar" />
+          <label>{i18n.t(locals.label)}</label>
+        </div>
       </div>
+      {/* {Error} */}
     </div>
   ) : null;
 }

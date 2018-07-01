248e41189fe81488889e9bb34c56a8a58f5abbe4
Sagar <sagararyal@users.noreply.github.com>
2018-07-01 11:54:58 +0200

    Update editUserProfilelabels.js

--- app/locales/en/editUserProfilelabels.js
+++ app/locales/en/editUserProfilelabels.js
@@ -1,10 +1,10 @@
 export default {
   sure: 'Are you sure ? ',
   delete_confirmation:
-    'Once you confirm, all of your account data will be deleted. this can not be undone',
-  password_changed: 'Your password has been successfully changed',
-  return_to_profile: 'Return to My profile',
-  new_project: 'Add new project',
+    'Once you confirm, all of your account data will be deleted. This can not be undone',
+  password_changed: 'Your password has been changed successfully',
+  return_to_profile: 'Return to My Profile',
+  new_project: 'Add New Project',
   Location: 'Location',
   description: 'Description',
   gallery: 'Image Gallery',

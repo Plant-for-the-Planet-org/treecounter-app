61d07d6622c138bb9c13d1efe52e63a5dbb50334
hkurra <er.harshkurra@gmail.com>
2018-07-05 01:07:14 +0530

    Fixed negative case for public tree counter if user will disable his public visibility

--- app/actions/treecounterLookupAction.js
+++ app/actions/treecounterLookupAction.js
@@ -3,11 +3,16 @@ import { normalize, denormalize } from 'normalizr';
 
 import { treecounterSchema } from '../schemas/index';
 import { mergeEntities } from '../reducers/entitiesReducer';
+import { updateRoute } from '../helpers/routerHelper';
 
 export function treecounterLookupAction(treecounterId) {
   return (dispatch, getState) => {
     // the treecounter might already exist in normalized form in the redux-store
     // if so we de-normalize and just return it
+    if (!treecounterId) {
+      updateRoute('app_homepage', dispatch);
+      return Promise.reject('user not available');
+    }
     return new Promise(function(resolve) {
       const entities = getState().entities;
       if (entities.treecounter.treecounterId) {
@@ -22,11 +27,16 @@ export function treecounterLookupAction(treecounterId) {
 
       // the treecounter has not been fetched yet
       // so we store it in normalized form in the redux-store but return the original data
-      getRequest('treecounter_get', { uid: treecounterId }).then(result => {
-        const treecounter = result.data;
-        dispatch(mergeEntities(normalize(treecounter, treecounterSchema)));
-        resolve(treecounter);
-      });
+      getRequest('treecounter_get', { uid: treecounterId })
+        .then(result => {
+          const treecounter = result.data;
+          dispatch(mergeEntities(normalize(treecounter, treecounterSchema)));
+          resolve(treecounter);
+        })
+        .catch(error => {
+          updateRoute('app_homepage', dispatch);
+          Promise.reject(error);
+        });
     });
   };
 }

5b48a97d1e82e527612236ce10f6f4884e9e5875
niteshgrg <niteshgarg.06@gmail.com>
2018-07-15 01:23:32 +0530

    make textarea template

--- app/components/Templates/TextAreaTemplate.native.js
+++ app/components/Templates/TextAreaTemplate.native.js
@@ -3,5 +3,45 @@ import React from 'react';
 import i18n from '../../locales/i18n';
 
 export function TextAreaTemplate(locals) {
-  return <View />;
+  let containerStyle = {
+    paddingTop: 20,
+    paddingBottom: 20,
+    height: 150
+  };
+  let textboxStyle = {
+    marginLeft: 10,
+    fontSize: 16,
+    color: '#000',
+    borderBottomWidth: 1,
+    borderColor: '#cecece',
+    height: 150
+  };
+  let errorBlockStyle = locals.stylesheet && locals.stylesheet.errorBlock;
+  let error =
+    locals.hasError && locals.error ? (
+      <Text accessibilityLiveRegion="polite" style={errorBlockStyle}>
+        {locals.error}
+      </Text>
+    ) : null;
+  return (
+    <View>
+      <View style={containerStyle}>
+        <TextInput
+          style={textboxStyle}
+          secureTextEntry={locals.secureTextEntry}
+          placeholder={i18n.t(locals.placeholder)}
+          keyboardType={locals.keyboardType}
+          maxLength={locals.maxLength}
+          multiline={6}
+          value={locals.value}
+          onChangeText={value => locals.onChange(value)}
+          onChange={locals.onChangeNative}
+          onKeyPress={locals.onKeyPress}
+          returnKeyType={locals.returnKeyType}
+          autoCapitalize={locals.autoCapitalize}
+        />
+      </View>
+      {error}
+    </View>
+  );
 }

47bb6ebd9d762a065df985005e43bc24be14e410
hkurra <er.harshkurra@gmail.com>
2018-07-19 01:44:02 +0530

    UI Improvements

--- app/components/FAQ/index.native.js
+++ app/components/FAQ/index.native.js
@@ -7,9 +7,11 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  Linking
 } from 'react-native';
-// import CardLayout from '../Common/Card/CardLayout';
+import LoadingIndicator from '../../components/Common/LoadingIndicator';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,21 +19,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF'
   },
   header: {
-    // backgroundColor: '#F5FCFF',
     borderTopLeftRadius: 5,
     borderTopRightRadius: 5,
     backgroundColor: '#fff',
     padding: 10,
     marginTop: 8,
     marginLeft: 8,
-    marginRight: 8,
-    textAlign: 'left',
-    color: '#686060'
+    marginRight: 8
   },
   headerText: {
     textAlign: 'center',
     fontSize: 16,
-    fontWeight: '500'
+    fontWeight: '500',
+    textAlign: 'left',
+    color: '#686060'
   },
   content: {
     borderBottomLeftRadius: 5,
@@ -40,14 +41,14 @@ const styles = StyleSheet.create({
     marginRight: 8,
     padding: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    color: '#938989'
+    backgroundColor: '#fff'
   },
 
   a: {
     fontWeight: '300',
     color: '#e86f56'
-  }
+  },
+  p: { color: '#938989' }
 });
 export default class FAQ extends Component {
   _renderHeader(section) {
@@ -61,12 +62,23 @@ export default class FAQ extends Component {
   _renderContent(section) {
     return (
       <View style={styles.content}>
-        <HTMLView value={section.answer} stylesheet={styles} />
+        <HTMLView
+          value={section.answer}
+          stylesheet={styles}
+          // onLinkPress={url => {
+          //   console.log('clicked link: ', url);
+          //   Linking.openURL(url).catch(err => {
+          //     console.log(err);
+          //   });
+          // }}
+        />
       </View>
     );
   }
   render() {
-    return (
+    return this.props.loading ? (
+      <LoadingIndicator />
+    ) : (
       <ScrollView>
         <Accordion
           sections={this.props.faqs}
@@ -81,5 +93,6 @@ export default class FAQ extends Component {
 }
 
 FAQ.propTypes = {
-  faqs: PropTypes.array.isRequired
+  faqs: PropTypes.array.isRequired,
+  loading: PropTypes.bool
 };

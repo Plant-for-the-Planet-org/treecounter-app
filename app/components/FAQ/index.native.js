8b571659a9ade1c9676543321f1131c9beeb5fd4
hkurra <er.harshkurra@gmail.com>
2018-07-19 13:33:28 +0530

    Moved faq css into separate file

--- app/components/FAQ/index.native.js
+++ app/components/FAQ/index.native.js
@@ -6,63 +6,17 @@ import { foldin, foldout } from '../../assets';
 import {
   Text,
   View,
-  StyleSheet,
   TouchableOpacity,
   ScrollView,
-  Image
+  Image,
+  Linking
 } from 'react-native';
 import LoadingIndicator from '../../components/Common/LoadingIndicator';
+import { context } from '../../config';
+import styles from '../../styles/faq';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  header: {
-    flexDirection: 'row',
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    padding: 12,
-    marginTop: 8,
-    marginLeft: 8,
-    marginRight: 8,
-    justifyContent: 'space-between'
-  },
-  headerText: {
-    textAlign: 'center',
-    fontSize: 16,
-    fontWeight: '500',
-    textAlign: 'left',
-    color: '#686060',
-    flex: 1
-  },
-  content: {
-    borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5,
-    marginLeft: 8,
-    marginRight: 8,
-    padding: 20,
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  imageStyle: {
-    width: 17,
-    height: 18,
-    resizeMode: 'center'
-  },
-
-  a: {
-    fontWeight: '300',
-    color: '#e86f56'
-  },
-  p: { color: '#938989' }
-});
 export default class FAQ extends Component {
   _renderHeader(section, index, isActive) {
-    console.log('_renderHeader', index, isActive);
     return (
       <View style={styles.header}>
         <Text style={styles.headerText}>{section.question}</Text>
@@ -77,12 +31,20 @@ export default class FAQ extends Component {
         <HTMLView
           value={section.answer}
           stylesheet={styles}
-          // onLinkPress={url => {
-          //   console.log('clicked link: ', url);
-          //   Linking.openURL(url).catch(err => {
-          //     console.log(err);
-          //   });
-          // }}
+          onLinkPress={url => {
+            try {
+              url = url.startsWith('/')
+                ? `${context.scheme}://${context.host}${url}`
+                : url;
+            } catch (err) {
+              console.log(err);
+            }
+
+            console.log('clicked link: ', url);
+            Linking.openURL(url).catch(err => {
+              console.log(err);
+            });
+          }}
         />
       </View>
     );

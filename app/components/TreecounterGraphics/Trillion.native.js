b5cd39a1de5cf8de753137631e66e68a4bed1e7d
niteshgrg <niteshgarg.06@gmail.com>
2018-07-25 21:39:49 +0530

    Circle colors calculating code

--- app/components/TreecounterGraphics/Trillion.native.js
+++ app/components/TreecounterGraphics/Trillion.native.js
@@ -5,19 +5,53 @@ import ballonsSvg from '../../assets/images/ballons.svg';
 import cloud1Svg from '../../assets/images/cloud1.svg';
 import cloud2Svg from '../../assets/images/cloud2.svg';
 import { svgBackground } from '../../assets';
+import { trillionCampaign } from '../../actions/trillionAction';
 import SvgUri from 'react-native-svg-uri';
+import Svg, { Circle } from 'react-native-svg';
 
 export default class Trillion extends Component {
   constructor() {
     super();
+    this.state = {
+      svgData: {},
+      displayName: '',
+      loading: true,
+      plantedDasharray: '0,1000'
+    };
     this.RotateBallonsValueHolder = new Animated.Value(0);
     this.RotateClouds1ValueHolder = new Animated.Value(0);
     this.RotateClouds2ValueHolder = new Animated.Value(0);
   }
   componentDidMount() {
+    trillionCampaign()
+      .then(({ data }) => {
+        let planted = this.calculatePlantedWidth(
+          data.countPlanted,
+          data.countTarget,
+          112
+        );
+        this.setState({
+          svgData: {
+            id: 1,
+            target: data.countTarget,
+            planted: data.countPlanted,
+            community: data.countCommunity,
+            personal: data.countPersonal
+          },
+          plantedDasharray: planted + ',1000',
+          displayName: data.displayName,
+          loading: false
+        });
+      })
+      .catch(error => console.log(error));
     this.StartBallonsRotateFunction();
     this.StartClouds1RotateFunction();
     this.StartClouds2RotateFunction();
+  }
+
+  calculatePlantedWidth(planted, target, radius) {
+    let total = 2 * 3.14 * radius;
+    return total / (1 + target / planted);
   }
 
   StartBallonsRotateFunction() {
@@ -76,19 +110,42 @@ export default class Trillion extends Component {
           <Animated.View style={{ transform: [{ rotate: RotateBallonsData }] }}>
             <SvgUri width="400" height="400" source={ballonsSvg} />
           </Animated.View>
-          <View style={styles.cloud1Style}>
+          <View style={styles.cloudStyle}>
             <Animated.View
               style={{ transform: [{ rotate: RotateClouds1Data }] }}
             >
               <SvgUri width="400" height="400" source={cloud1Svg} />
             </Animated.View>
           </View>
-          <View style={styles.cloud1Style}>
+          <View style={styles.cloudStyle}>
             <Animated.View
               style={{ transform: [{ rotate: RotateClouds2Data }] }}
             >
               <SvgUri width="400" height="400" source={cloud2Svg} />
             </Animated.View>
+          </View>
+          <View style={styles.circleStyle}>
+            <Svg height="400" width="400">
+              <Circle
+                cx="200"
+                cy="197"
+                r="112"
+                fill="none"
+                stroke="#c4d19a"
+                strokeWidth="30"
+              />
+              <Circle
+                cx="200"
+                cy="197"
+                r="112"
+                fill="none"
+                stroke="#4d6c29"
+                rotation="-90"
+                origin="200, 197"
+                strokeWidth="30"
+                strokeDasharray={this.state.plantedDasharray}
+              />
+            </Svg>
           </View>
         </View>
       </View>
@@ -114,11 +171,19 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10
   },
-  cloud1Style: {
+  cloudStyle: {
     flex: 1,
     width: 400,
     height: 400,
     elevation: 2,
+    position: 'absolute',
+    top: 0
+  },
+  circleStyle: {
+    flex: 1,
+    width: 400,
+    height: 400,
+    elevation: 3,
     position: 'absolute',
     top: 0
   },

c8d01d397b2d70e69ddf756d34e2b0ae28c34011
hkurra <er.harshkurra@gmail.com>
2018-07-31 01:06:40 +0530

    missed in last commit

---
+++ app/styles/myTrees/my_trees.native.js
@@ -0,0 +1,33 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default (myTreesStyle = EStyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    paddingVertical: 20
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    flex: 1,
+    flexGrow: 1
+  },
+  headContainer: {
+    padding: 30
+  },
+  mapContainer: {
+    width: '100%',
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  listContainer: { backgroundColor: 'white' },
+  contributionMapLegend: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 5,
+    alignSelf: 'flex-end'
+  }
+}));

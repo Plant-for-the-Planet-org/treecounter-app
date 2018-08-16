cf5168efb9f194d7e0c546b47eb417796d070dfa
niteshgrg <niteshgarg.06@gmail.com>
2018-08-16 17:24:14 +0530

    Fix loading indicator layout

--- app/components/Common/LoadingIndicator.native.js
+++ app/components/Common/LoadingIndicator.native.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image, View } from 'react-native';
 import { loadingBar } from '../../assets';
 import i18n from '../../locales/i18n.js';
+import { Dimensions } from 'react-native';
 
 const LoadingIndicator = () => (
   <View style={loadingIndicatorStyle}>
@@ -11,6 +12,8 @@ const LoadingIndicator = () => (
 
 const loadingIndicatorStyle = {
   flex: 1,
+  height: Dimensions.get('window').height,
+  width: Dimensions.get('window').width,
   justifyContent: 'center',
   alignItems: 'center',
   flexDirection: 'column'

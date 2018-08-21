c44ed4d91466ca68210e9e1d9a8df4b60c2bad92
hkurra <er.harshkurra@gmail.com>
2018-08-22 00:51:05 +0530

    Integrated Toast message to show response of every network request

--- app/notification/PopupNotificaiton/notificationManager.native.js
+++ app/notification/PopupNotificaiton/notificationManager.native.js
@@ -1,12 +1,30 @@
+import Toast from 'react-native-root-toast';
+
+const getToastConfig = function(timeout) {
+  return {
+    duration: timeout ? timeout : Toast.durations.LONG,
+    position: 80,
+    shadow: false,
+    animation: true,
+    hideOnPress: true,
+    delay: 0
+  };
+};
 class _NotificationManager {
   success(message, title, timeOut) {
     console.log(message, title);
-    //TODO hkurra implement this using native snack bar on platform
+    Toast.show(title + (message ? '\n ' + message : ''), {
+      ...getToastConfig(timeOut),
+      backgroundColor: 'rgba(90, 159, 70, 1)'
+    });
   }
 
   error(message, title, timeOut) {
     console.log(message, title);
-    //TODO hkurra implement this using native snack bar on platform
+    Toast.show(title + (message ? '\n ' + message : ''), {
+      ...getToastConfig(timeOut),
+      backgroundColor: 'rgba(171, 51, 50, 1)'
+    });
   }
 }
 

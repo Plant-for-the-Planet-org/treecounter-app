d817a07098afd4d2a21b3b11c72955816df24650
Jorgo Miridis <jorgo@miridis.com>
2018-08-28 17:10:54 -0500

    fix circleci config

--- .circleci/test_config.sh
+++ .circleci/test_config.sh
@@ -3,7 +3,7 @@
 curl -k \
     --user 22aeeef20e8b0e7a83caac3160cbeb9dc606233a: \
     --request POST \
-    --form revision=f2d3b6dfa038dd27cb7a8a468229d48a677a4337 \
+    --form revision=cd1e43b887aaedc8f7db7d5374a00a9ce885cb54 \
     --form config=@.circleci/config.yml \
     --form notify=false  \
     https://circleci.com/api/v1.1/project/github/Plant-for-the-Planet-org/treecounter-app/tree/staging

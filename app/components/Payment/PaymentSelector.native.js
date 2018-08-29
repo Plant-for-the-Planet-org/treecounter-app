63e816d933b23d072a72b364a594726e392dcb08
Karan Hudia <karanhudia@gmail.com>
2018-08-30 01:35:01 +0530

    payments changes

--- app/components/Payment/PaymentSelector.native.js
+++ app/components/Payment/PaymentSelector.native.js
@@ -5,23 +5,67 @@ import PropTypes from 'prop-types';
 
 import Paypal from './Gateways/Paypal';
 import { View } from 'react-native';
+import { foldin, foldout } from '../../assets';
+import Accordion from 'react-native-collapsible/Accordion';
 
 class PaymentSelector extends React.Component<{}, { elementFontSize: string }> {
   constructor(props) {
     super(props);
   }
+  _renderHeader(section, index, isActive) {
+    return (
+      <View style={styles.header}>
+        <Text style={styles.headerText}>{section.gateway}</Text>
+        <Image style={styles.imageStyle} source={isActive ? foldin : foldout} />
+      </View>
+    );
+  }
+
+  _renderContent(section) {
+    const { accounts, paymentMethods, amount, currency, context } = this.props;
+    const gatewayProps = {
+      context: context,
+      currency: currency,
+      onFailure: this.props.onFailure,
+      onError: this.props.onError
+    };
+    let paymentGateway = section.gateway;
+    let content = 'simple';
+    if ('paypal' === paymentGateway) {
+      content = (
+        <Paypal
+          key={gateway}
+          amount={this.props.amount}
+          currency={this.props.currency}
+          account={accounts[paymentGateway]}
+          {...gatewayProps}
+        />
+      );
+    }
+    return;
+    <View style={styles.content}>{content}</View>;
+  }
 
   render() {
     const { accounts, paymentMethods, amount, currency, context } = this.props;
-    const accountName = paymentMethods ? paymentMethods['paypal'] : null;
+    const gatewayProps = {
+      context: context,
+      currency: currency,
+      onFailure: this.props.onFailure,
+      onError: this.props.onError
+    };
     return (
       <View>
-        <Paypal
-          amount={amount}
-          currency={currency}
-          account={accounts[accountName]}
-          expanded={this.props.expandedOption === '3'}
-          handleExpandedClicked={this.handleExpandedClicked}
+        <View>
+          <Text>
+            Amount: {amount} {currency}
+          </Text>
+        </View>
+        <Accordion
+          sections={this.props.paymentMethods}
+          renderHeader={this._renderHeader}
+          renderContent={this._renderContent}
+          touchableComponent={TouchableOpacity}
         />
       </View>
     );

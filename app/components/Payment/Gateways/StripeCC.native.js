520f85d9dbbd5bad5f1ecbd02f98d3a52f2be92f
shubhi <karanhudia@gmail.com>
2018-09-02 01:29:28 +0530

    Fixed dependecy issue in stripecss payment screen.

--- app/components/Payment/Gateways/StripeCC.native.js
+++ app/components/Payment/Gateways/StripeCC.native.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { PaymentCardTextField } from 'tipsi-stripe';
 
 const styles = StyleSheet.create({

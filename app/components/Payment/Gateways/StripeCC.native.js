ec60d68626a1fb72a8444ed358482cab43a7d738
shubhi <karanhudia@gmail.com>
2018-09-02 11:56:00 +0530

    StripeCC card changes...

--- app/components/Payment/Gateways/StripeCC.native.js
+++ app/components/Payment/Gateways/StripeCC.native.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { PaymentCardTextField } from 'tipsi-stripe';
 
 const styles = StyleSheet.create({
@@ -13,23 +13,45 @@ const styles = StyleSheet.create({
 });
 
 export default class FieldExample extends Component {
+  state = {
+    valid: false,
+    params: {
+      number: '',
+      expMonth: 0,
+      expYear: 0,
+      cvc: ''
+    }
+  };
+
   handleFieldParamsChange = (valid, params) => {
-    console.log(`
-      Valid: ${valid}
-      Number: ${params.number || '-'}
-      Month: ${params.expMonth || '-'}
-      Year: ${params.expYear || '-'}
-      CVC: ${params.cvc || '-'}
-    `);
+    this.setState({
+      valid,
+      params
+    });
   };
 
   render() {
+    const { valid, params } = this.state;
     return (
       <View>
+        <Text style={styles.header}>PaymentCardTextField Example</Text>
         <PaymentCardTextField
+          accessible={false}
           style={styles.field}
           onParamsChange={this.handleFieldParamsChange}
+          numberPlaceholder="XXXX XXXX XXXX XXXX"
+          expirationPlaceholder="MM/YY"
+          cvcPlaceholder="CVC"
         />
+        <View style={styles.params}>
+          <Text style={styles.instruction}>Valid:</Text>
+          <Text style={styles.instruction}>Number: {params.number || '-'}</Text>
+          <Text style={styles.instruction}>
+            Month: {params.expMonth || '-'}
+          </Text>
+          <Text style={styles.instruction}>Year: {params.expYear || '-'}</Text>
+          <Text style={styles.instruction}>CVC: {params.cvc || '-'}</Text>
+        </View>
       </View>
     );
   }

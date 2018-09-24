5aa6e13b0076927f15e6d9a7aa7dd91f3f61a193
hkurra <harshkurra21@gmail.com>
2018-09-24 21:06:11 +0530

    More changes for basic set up in widgets

--- widget.js
+++ widget.js
@@ -1,1 +1,7 @@
-console.log('PFTP Widgets under development');
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('PFTP Widgets under development');
+  let allBlockQuote = document.getElementsByTagName('blockquote');
+  for (let i = 0; i < allBlockQuote.length; i++) {
+    console.log(allBlockQuote[i]);
+  }
+});

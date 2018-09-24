43ac53071d47f333f9e7f39280ce1366c4012f21
Aasaqt <aasaqt01@gmail.com>
2018-09-24 23:36:59 +0530

    Prioritizing euro, dollar and pounds in currencies

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -3,8 +3,23 @@ import { getRequest } from '../utils/api';
 
 export function fetchCurrencies() {
   return dispatch => {
-    getRequest('public_currencies_get').then(response =>
-      dispatch(setCurrenciesConversions(response.data))
-    );
+    getRequest('public_currencies_get').then(response => {
+      let currency_names = response.data.currency_names;
+      let usd = currency_names['USD'];
+      let euro = currency_names['EUR'];
+      let pound = currency_names['LBP'];
+      let newCurrencies = {};
+      newCurrencies['USD'] = usd;
+      newCurrencies['EUR'] = euro;
+      newCurrencies['LBP'] = pound;
+      currency_names = _.omit(currency_names, 'USD');
+      currency_names = _.omit(currency_names, 'EUR');
+      currency_names = _.omit(currency_names, 'LBP');
+      Object.keys(currency_names).map(value => {
+        newCurrencies[value] = currency_names[value];
+      });
+      response.data.currency_names = newCurrencies;
+      dispatch(setCurrenciesConversions(response.data));
+    });
   };
 }

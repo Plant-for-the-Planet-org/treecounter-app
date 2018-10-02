84c2f4c64ebe20b2462addda7281ad52140e7cc5
hkurra <er.harshkurra@gmail.com>
2018-10-02 18:26:46 +0530

    Created shadow-Dom instead of normal one for Widget to avoid any css bledding from parent DOM

--- web/widgets/basic/widget.js
+++ web/widgets/basic/widget.js
@@ -2,10 +2,14 @@ import { getApiRoute } from '../../../app/actions/apiRouting';
 import axios from 'axios';
 import { context } from '../../../app/config';
 import { tree_outline } from '../../../app/assets';
-const widget = require('./basic.widget.html');
 import './basic.widget.scss';
-
+const widget = require('./basic.widget.html');
 const { scheme, host, base: baseUrl } = context;
+
+const serverName = `${scheme}://${host}`;
+const cssStyle = `<link href="widget.css" rel="stylesheet"/>
+  <link href="${serverName}/widget.css" rel="stylesheet"/>`;
+
 export async function getRequest(route, params) {
   let url = await getApiRoute(route, params);
   return await axios
@@ -30,19 +34,24 @@ document.addEventListener('DOMContentLoaded', function() {
         getRequest('treecounter_get', { uid })
           .then(result => {
             const { data } = result;
+            const header = document.createElement('header');
+
             let div = document.createElement('div');
-            div.innerHTML = widget
-              .replace('${tree-count}', result.data.countPersonal)
-              .replace('${user}', result.data.displayName)
-              .replace('${img-src}', tree_outline)
-              .replace('${user-id}', uid);
+            const shadowRoot = div.attachShadow({ mode: 'closed' });
+            shadowRoot.innerHTML =
+              cssStyle +
+              widget
+                .replace('${tree-count}', result.data.countPersonal)
+                .replace('${user}', result.data.displayName)
+                .replace('${img-src}', tree_outline)
+                .replace('${user-id}', uid);
             console.log(result);
             allBlockQuote[i].parentNode.insertBefore(div, allBlockQuote[i]);
             allBlockQuote[i].parentNode.removeChild(allBlockQuote[i]);
             window.pftp = {
               giftTree: event => {
                 console.log(event);
-                const serverName = `${scheme}://${host}`;
+
                 const uid = event.target.id;
                 const url = `${serverName}${baseUrl}/giftTrees?uid=${uid}`;
                 console.log(serverName);

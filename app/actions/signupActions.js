8f1375c4db52d125db2bdaf25b6261e359693418
niteshgrg <niteshgarg.06@gmail.com>
2018-11-09 19:36:49 +0530

    Add api call to signup action as well

--- app/actions/signupActions.js
+++ app/actions/signupActions.js
@@ -2,7 +2,7 @@ import { NotificationManager } from '../notification/PopupNotificaiton/notificat
 
 import { updateRoute } from '../helpers/routerHelper';
 import { postRequest } from '../utils/api';
-import { updateJWT } from '../utils/user';
+import { updateJWT, updateActivateToken } from '../utils/user';
 import { loadUserProfile } from './loadUserProfileAction';
 import { setProgressModelState } from '../reducers/modelDialogReducer';
 
@@ -13,13 +13,18 @@ export function signUp(profileType, userData) {
       postRequest('signup_post', userData, { profileType: profileType })
         .then(res => {
           const { token, refresh_token, data } = res.data;
-          updateJWT(token, refresh_token);
-          dispatch(loadUserProfile());
-          NotificationManager.success(
-            'Registration Successful',
-            'Congrats',
-            5000
-          );
+          if (!data.isActivated) {
+            updateActivateToken(userData.email, token);
+          } else {
+            updateJWT(token, refresh_token);
+            dispatch(loadUserProfile());
+            NotificationManager.success(
+              'Registration Successful',
+              'Congrats',
+              5000
+            );
+          }
+
           updateRoute(data.routeName, dispatch, null, data.routeParams);
           dispatch(setProgressModelState(false));
         })

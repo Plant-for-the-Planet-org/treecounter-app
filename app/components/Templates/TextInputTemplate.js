2de5c438502f6b6143d9a616b4bcb9c4e748433a
hkurra <er.harshkurra@gmail.com>
2018-11-24 01:36:44 +0530

    Material design for text inputs are back

--- app/components/Templates/TextInputTemplate.js
+++ app/components/Templates/TextInputTemplate.js
@@ -3,6 +3,7 @@ import i18n from '../../locales/i18n';
 import { formatDate } from '../../helpers/utils';
 
 export function TextInputTemplate(locals) {
+  const locale = getLocale();
   function onChange($event) {
     let value =
       locals.type === 'number' && $event.target.value
@@ -23,7 +24,7 @@ export function TextInputTemplate(locals) {
   } else {
     className = 'pftp-textfield__error-inputgroup';
   }
-
+  const isDate = locals.type === 'date';
   return locals.type !== 'hidden' ? (
     <div className="pftp-textfield-container">
       <div className="pftp-textfield">
@@ -31,8 +32,10 @@ export function TextInputTemplate(locals) {
           <img className="pftp-textfield__icon" src={locals.config.iconUrl} />
         ) : null}
         <div className={className}>
-          {locals.type === 'date' ? (
+          {isDate ? (
             <input
+              date-format="dd/mm/yyyy"
+              lang={locale}
               type={locals.type}
               autoComplete="new-password"
               max={todayDate()}
@@ -62,7 +65,9 @@ export function TextInputTemplate(locals) {
                 : 'pftp-textfield__inputgroup--error-bar'
             }
           />
-          <label>{i18n.t(locals.label)}</label>
+          <label className={locals.value || isDate ? 'float-label' : ''}>
+            {i18n.t(locals.label)}
+          </label>
         </div>
       </div>
       {error && locals.error ? locals.error : null}

9e67a9debb0cd31584cee5cbd14d58b61f9acf24
hkurra <er.harshkurra@gmail.com>
2018-11-24 16:17:31 +0530

    Missed

---
+++ app/reducers/updateLastRouteReducer.js
@@ -0,0 +1,19 @@
+import { createAction, handleActions } from 'redux-actions';
+
+export const setLastRoute = createAction('SET_LAST_ROUTE');
+export const getLastRoute = state => state.lastRoute;
+
+export const initialState = {
+  lastRoute: ''
+};
+
+const updateLastRouteReducer = handleActions(
+  {
+    [setLastRoute]: (state, action) => ({
+      lastRoute: action.payload
+    })
+  },
+  initialState
+);
+
+export default updateLastRouteReducer;

5af7b75793919dcee1355b52736a4bb903b716bc
hkurra <er.harshkurra@gmail.com>
2018-11-26 00:02:18 +0530

    reverted config file

--- app/config/index.js
+++ app/config/index.js
@@ -11,8 +11,8 @@ export const initialProps = {
 
 export const context = {
   scheme: 'https', // API server protocol
-  host: 'www.trilliontreecampaign.org', // API server domain
-  base: '', // debug mode on/off, set to empty string to switch debug mode off
+  host: 'test.trilliontreecampaign.org', // API server domain
+  base: '/app_dev.php', // debug mode on/off, set to empty string to switch debug mode off
   debug: true, // local console debugging switch
   currency: 'USD',
   mapIds: { inventory: 'dee6acf9de774fe6878813f707b4ab88' }

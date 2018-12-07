2d37761fb992ad85794e582c2dbe264c7dec9edd
niteshgrg <niteshgarg.06@gmail.com>
2018-12-07 08:54:02 +0530

    Adapt to new syntax

--- downloadForms.js
+++ downloadForms.js
@@ -1,26 +1,38 @@
-import axios from 'axios';
-import { getRequest } from './app/utils/api';
+const axios = require('axios');
 const fs = require('fs');
 
 const list = [
   {
-    api: 'https://www.trilliontreecampaign.org/api/v1.0/en/resetPassword',
-    file: 'resetPassword.js'
+    api: 'https://www.trilliontreecampaign.org/public/v1.0/en/forms',
+    file: 'signup.js'
   }
 ];
 
 for (let i in list) {
-  getRequest(list[i]['api']).then(response => {
-    fs.writeFile(
-      './app/server/formSchemas/' + list[i]['file'],
-      'export default ' + response,
-      function(err) {
-        if (err) {
-          return console.log(err);
-        }
-
-        console.log('The file was saved!');
+  axios
+    .get(list[i]['api'])
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        return response;
+      } else {
+        let error = new Error(response);
+        throw error;
       }
-    );
-  });
+    })
+    .then(response => {
+      console.log(JSON.stringify(response.data.targetUpdate_form.data.schema));
+      fs.writeFile(
+        './app/server/formSchemas/' + list[i]['file'],
+        'export default ' +
+          JSON.stringify(response.data.targetUpdate_form.data.schema),
+        function(err) {
+          if (err) {
+            return console.log(err);
+          }
+
+          console.log('The file was saved!');
+        }
+      );
+    })
+    .catch(err => console.log(err));
 }

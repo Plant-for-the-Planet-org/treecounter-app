011e2609d79a87a3720242c65975c9d6549d73d1
hkurra <er.harshkurra@gmail.com>
2019-01-08 22:55:24 +0530

    Implemented Global Error Handler for app to catch any unhandled exception in app

--- index.native.js
+++ index.native.js
@@ -1,32 +1,35 @@
 import { AppRegistry, Platform } from 'react-native';
 import App from './app/components/App';
 import './ReactotronConfig';
+import stacktraceParser from 'stacktrace-parser';
+import { Alert } from 'react-native';
 /* app.js */
 
 console.disableYellowBox = true;
 
-// import {
-//   setCustomView,
-//   setCustomTextInput,
-//   setCustomText,
-//   setCustomImage,
-//   setCustomTouchableHighlight
-// } from 'react-native-global-props';
+const parseErrorStack = error => {
+  if (!error || !error.stack) {
+    return [];
+  }
+  return Array.isArray(error.stack)
+    ? error.stack
+    : stacktraceParser.parse(error.stack);
+};
 
-// const customTextProps = {
-//   style: {
-//     fontFamily: Platform.OS === 'ios' ? 'HelveticaNeue' : 'Roboto'
-//   }
-// };
+// intercept react-native error handling
+if (ErrorUtils.getGlobalHandler) {
+  console.log('ErrorUtils._globalHandler', ErrorUtils._globalHandler);
+  this.defaultHandler =
+    (ErrorUtils.getGlobalHandler && ErrorUtils.getGlobalHandler()) ||
+    ErrorUtils._globalHandler;
+  ErrorUtils.setGlobalHandler(wrapGlobalHandler); // feed errors directly to our wrapGlobalHandler function
+}
 
-// // Makes every image resize mode cover by default.
-// const customImageProps = {
-//   resizeMode: 'cover'
-// };
-
-// //setCustomTextInput(customTextInputProps);
-// setCustomText(customTextProps);
-// setCustomImage(customImageProps);
-//setCustomTouchableHighlight(customTouchableOpacityProps);
+async function wrapGlobalHandler(error, isFatal) {
+  const stack = parseErrorStack(error);
+  console.log(error, stack, isFatal);
+  Alert.alert('Error', 'Something went wrong.');
+  this.defaultHandler(error, isFatal); //after you're finished, call the defaultHandler so that react-native also gets the error
+}
 
 AppRegistry.registerComponent('TreecounterApp', () => App);

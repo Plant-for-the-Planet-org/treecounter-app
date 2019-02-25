1c703c6fb19f2460fffff55ffe2b23c0324c2d3d
Aasaqt <aasaqt01@gmail.com>
2019-02-26 01:20:32 +0530

    My trees as per documentation

--- app/components/UserContributions/ContributionCard.js
+++ app/components/UserContributions/ContributionCard.js
@@ -122,9 +122,12 @@ export default class ContributionCard extends React.Component {
   }
   render() {
     let { contribution } = this.props;
-    let imagesArray = contribution.contributionImages.map(image => {
-      return { src: getImageUrl('contribution', 'medium', image.image) };
-    });
+    let imagesArray =
+      contribution.category === 'contributions'
+        ? contribution.contributionImages.map(image => {
+            return { src: getImageUrl('contribution', 'medium', image.image) };
+          })
+        : '';
     let seeLabel = classnames('see-more-label-style', {
       'see-more__active': this.state.viewExpanded
     });
@@ -138,9 +141,11 @@ export default class ContributionCard extends React.Component {
       givee,
       giveeSlug,
       tpoName,
+      isPending,
       mayUpdate,
       cardType,
       contributionType,
+      category,
       registrationDate,
       redemptionCode,
       redemptionDate
@@ -176,7 +181,7 @@ export default class ContributionCard extends React.Component {
         : treeCount > 1
           ? '#68aeec'
           : '#ec6453';
-    return (
+    return category === 'contributions' && contributionType === 'donation' ? (
       <div>
         <div
           style={{
@@ -194,53 +199,64 @@ export default class ContributionCard extends React.Component {
           }`}
         >
           <div className="contribution-container__left-column">
-            <TextSpan strong={true}>
-              {contribution.treeCount + ' '}
-              {contribution.treeSpecies ? contribution.treeSpecies : ''}
-              {i18n.t('label.tree')}
-            </TextSpan>
-            <TextSpan>
-              {contribution.geoLatitude + ', ' + contribution.geoLongitude}
-            </TextSpan>
-            <TextSpan>
-              {moment(new Date(contribution.plantDate)).format('DD MMM YYYY')}
-            </TextSpan>
-            {imagesArray.length ? (
-              <a onClick={this.openLightbox}>{i18n.t('label.pictures')}</a>
+            {treeCountLine ? (
+              <TextSpan strong={true}>{treeCountLine}</TextSpan>
             ) : null}
-            <Lightbox
-              currentImage={this.state.currentImage}
-              images={imagesArray}
-              isOpen={this.state.lightboxIsOpen}
-              onClose={this.closeLightbox}
-              onClickNext={this.gotoNext}
-              onClickPrev={this.gotoPrevious}
-            />
+
+            {plantProjectLine ? <TextSpan>{plantProjectLine}</TextSpan> : null}
+            {donateActionLine ? <TextSpan>{donateActionLine}</TextSpan> : null}
+            {tpoLine ? <TextSpan>{tpoLine}</TextSpan> : null}
+            {/*<TextSpan strong={true}>*/}
+            {/*{contribution.treeCount + ' '}*/}
+            {/*{contribution.treeSpecies ? contribution.treeSpecies : ''}*/}
+            {/*{i18n.t('label.tree')}*/}
+            {/*</TextSpan>*/}
+            {/*<TextSpan>*/}
+            {/*{contribution.geoLatitude + ', ' + contribution.geoLongitude}*/}
+            {/*</TextSpan>*/}
+            {/*<TextSpan>*/}
+            {/*{moment(new Date(contribution.plantDate)).format('DD MMM YYYY')}*/}
+            {/*</TextSpan>*/}
+            {/*{imagesArray.length ? (*/}
+            {/*<a onClick={this.openLightbox}>{i18n.t('label.pictures')}</a>*/}
+            {/*) : null}*/}
+            {/*<Lightbox*/}
+            {/*currentImage={this.state.currentImage}*/}
+            {/*images={imagesArray}*/}
+            {/*isOpen={this.state.lightboxIsOpen}*/}
+            {/*onClose={this.closeLightbox}*/}
+            {/*onClickNext={this.gotoNext}*/}
+            {/*onClickPrev={this.gotoPrevious}*/}
+            {/*/>*/}
           </div>
           <div className="contribution-container__right-column">
-            {contribution.contributionMeasurements
-              .slice(0, 3)
-              .map(measurement => (
-                <TextSpan key={measurement.id}>
-                  {contribution.plantDate === measurement.measurementDate
-                    ? i18n.t('label.planting_day')
-                    : new Date(
-                        measurement.measurementDate
-                      ).toLocaleDateString() +
-                      (measurement.diameter + 'cm').padStart(10) +
-                      ((measurement.height / 100).toFixed(1) + 'm').padStart(
-                        10
-                      )}
-                </TextSpan>
-              ))}
-            {contribution.contributionMeasurements.length > 3 ? (
+            {contribution.category === 'contributions'
+              ? contribution.contributionMeasurements
+                  .slice(0, 3)
+                  .map(measurement => (
+                    <TextSpan key={measurement.id}>
+                      {contribution.plantDate === measurement.measurementDate
+                        ? i18n.t('label.planting_day')
+                        : new Date(
+                            measurement.measurementDate
+                          ).toLocaleDateString() +
+                          (measurement.diameter + 'cm').padStart(10) +
+                          (
+                            (measurement.height / 100).toFixed(1) + 'm'
+                          ).padStart(10)}
+                    </TextSpan>
+                  ))
+              : null}
+            {contribution.category === 'contributions' &&
+            contribution.contributionMeasurements.length > 3 ? (
               <div className={seeLabel} onClick={this.onViewExpanded}>
                 {this.state.viewExpanded
                   ? i18n.t('label.see_less')
                   : '+ ' + i18n.t('label.see_more')}
               </div>
             ) : null}
-            {this.state.viewExpanded
+            {this.state.viewExpanded &&
+            contribution.category === 'contributions'
               ? contribution.contributionMeasurements
                   .slice(3)
                   .map(measurement => (
@@ -255,8 +271,11 @@ export default class ContributionCard extends React.Component {
                     </TextSpan>
                   ))
               : null}
-            {contribution.contributionType == 'donation' ? (
-              <TextSpan> {i18n.t('label.donation_contribution')}</TextSpan>
+            {!isPending ? (
+              <TextSpan>
+                {' '}
+                {cardType.charAt(0).toUpperCase() + cardType.slice(1)}
+              </TextSpan>
             ) : (
               <Link
                 to={getLocalRoute('app_editTrees', {
@@ -270,7 +289,177 @@ export default class ContributionCard extends React.Component {
         </div>
         <hr className="contribution-container__partition" />
       </div>
-    );
+    ) : category === 'contributions' && contributionType === 'planting' ? (
+      <div>
+        <div
+          style={{
+            borderLeft:
+              '5px solid ' +
+              (contribution.contributionType == 'donation'
+                ? '#95c243'
+                : contribution.treeCount > 1
+                  ? '#68aeec'
+                  : '#ec6453')
+          }}
+          key={`contribution-${contribution.id}`}
+          className={`contribution-container__card ${
+            contribution.contributionType
+          }`}
+        >
+          <div className="contribution-container__left-column">
+            {treeCountLine ? (
+              <TextSpan strong={true}>{treeCountLine}</TextSpan>
+            ) : null}
+            {plantProjectLine ? <TextSpan>{plantProjectLine}</TextSpan> : null}
+            {plantActionLine ? <TextSpan>{plantActionLine}</TextSpan> : null}
+            {dedicateActionLine ? (
+              <TextSpan>{dedicateActionLine}</TextSpan>
+            ) : null}
+          </div>
+          <div className="contribution-container__right-column">
+            {contribution.category === 'contributions'
+              ? contribution.contributionMeasurements
+                  .slice(0, 3)
+                  .map(measurement => (
+                    <TextSpan key={measurement.id}>
+                      {contribution.plantDate === measurement.measurementDate
+                        ? i18n.t('label.planting_day')
+                        : new Date(
+                            measurement.measurementDate
+                          ).toLocaleDateString() +
+                          (measurement.diameter + 'cm').padStart(10) +
+                          (
+                            (measurement.height / 100).toFixed(1) + 'm'
+                          ).padStart(10)}
+                    </TextSpan>
+                  ))
+              : null}
+            {contribution.category === 'contributions' &&
+            contribution.contributionMeasurements.length > 3 ? (
+              <div className={seeLabel} onClick={this.onViewExpanded}>
+                {this.state.viewExpanded
+                  ? i18n.t('label.see_less')
+                  : '+ ' + i18n.t('label.see_more')}
+              </div>
+            ) : null}
+            {this.state.viewExpanded &&
+            contribution.category === 'contributions'
+              ? contribution.contributionMeasurements
+                  .slice(3)
+                  .map(measurement => (
+                    <TextSpan key={measurement.id}>
+                      {new Date(
+                        measurement.measurementDate
+                      ).toLocaleDateString() +
+                        (measurement.diameter + 'cm').padStart(10) +
+                        ((measurement.height / 100).toFixed(1) + 'm').padStart(
+                          10
+                        )}
+                    </TextSpan>
+                  ))
+              : null}
+            {!isPending ? (
+              <TextSpan>
+                {' '}
+                {cardType.charAt(0).toUpperCase() + cardType.slice(1)}
+              </TextSpan>
+            ) : (
+              <Link
+                to={getLocalRoute('app_editTrees', {
+                  contribution: contribution.id
+                })}
+              >
+                {i18n.t('label.update')}
+              </Link>
+            )}
+          </div>
+        </div>
+        <hr className="contribution-container__partition" />
+      </div>
+    ) : category === 'gifts' ? (
+      <div>
+        <div
+          style={{
+            borderLeft:
+              '5px solid ' +
+              (contribution.contributionType == 'donation'
+                ? '#95c243'
+                : contribution.treeCount > 1
+                  ? '#68aeec'
+                  : '#ec6453')
+          }}
+          key={`contribution-${contribution.id}`}
+          className={`contribution-container__card ${
+            contribution.contributionType
+          }`}
+        >
+          <div className="contribution-container__left-column">
+            {treeCountLine ? <TextSpan>{treeCountLine}</TextSpan> : null}
+            {plantProjectLine ? <TextSpan>{plantProjectLine}</TextSpan> : null}
+            {redeemActionLine ? <TextSpan>{redeemActionLine}</TextSpan> : null}
+            {tpoLine ? <TextSpan>{tpoLine}</TextSpan> : null}
+          </div>
+          <div className="contribution-container__right-column">
+            {contribution.category === 'contributions'
+              ? contribution.contributionMeasurements
+                  .slice(0, 3)
+                  .map(measurement => (
+                    <TextSpan key={measurement.id}>
+                      {contribution.plantDate === measurement.measurementDate
+                        ? i18n.t('label.planting_day')
+                        : new Date(
+                            measurement.measurementDate
+                          ).toLocaleDateString() +
+                          (measurement.diameter + 'cm').padStart(10) +
+                          (
+                            (measurement.height / 100).toFixed(1) + 'm'
+                          ).padStart(10)}
+                    </TextSpan>
+                  ))
+              : null}
+            {contribution.category === 'contributions' &&
+            contribution.contributionMeasurements.length > 3 ? (
+              <div className={seeLabel} onClick={this.onViewExpanded}>
+                {this.state.viewExpanded
+                  ? i18n.t('label.see_less')
+                  : '+ ' + i18n.t('label.see_more')}
+              </div>
+            ) : null}
+            {this.state.viewExpanded &&
+            contribution.category === 'contributions'
+              ? contribution.contributionMeasurements
+                  .slice(3)
+                  .map(measurement => (
+                    <TextSpan key={measurement.id}>
+                      {new Date(
+                        measurement.measurementDate
+                      ).toLocaleDateString() +
+                        (measurement.diameter + 'cm').padStart(10) +
+                        ((measurement.height / 100).toFixed(1) + 'm').padStart(
+                          10
+                        )}
+                    </TextSpan>
+                  ))
+              : null}
+            {!isPending ? (
+              <TextSpan>
+                {' '}
+                {cardType.charAt(0).toUpperCase() + cardType.slice(1)}
+              </TextSpan>
+            ) : (
+              <Link
+                to={getLocalRoute('app_editTrees', {
+                  contribution: contribution.id
+                })}
+              >
+                {i18n.t('label.update')}
+              </Link>
+            )}
+          </div>
+        </div>
+        <hr className="contribution-container__partition" />
+      </div>
+    ) : null;
   }
 }
 

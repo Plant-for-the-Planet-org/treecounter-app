7d9d65ef0c9603c117229919028c87017c6a0846
hkurra <er.harshkurra@gmail.com>
2019-03-03 02:58:41 +0530

    New design for TPO Manage project

--- app/components/ManageProjects/index.js
+++ app/components/ManageProjects/index.js
@@ -1,17 +1,102 @@
-import React, { Component } from 'react';
+import React, { Component, Children } from 'react';
 import PropTypes from 'prop-types';
 import PrimaryButton from '../Common/Button/PrimaryButton';
+import SecondaryButton from '../Common/Button/SecondaryButton';
 import t from 'tcomb-form';
 import i18n from '../../locales/i18n.js';
 import { plantProjectSchema } from '../../server/parsedSchemas/editProfile';
 import PlantProjectTemplate from '../EditUserProfile/PlantProjectTemplate';
+import { EditOrange, close_green, MapPinRed } from '../../assets';
+import _ from 'lodash';
 
 let TCombForm = t.form.Form;
 const emptyProjectInfo = {
-  name: '',
-  location: 'Campeche',
-  country: 'MX',
-  geoLocation: 'geoLongitude=-146.31011&geoLatitude=79.074456&country=MX'
+  name: ''
+  // location: 'Campeche',
+  // country: 'MX',
+  // geoLocation: 'geoLongitude=-146.31011&geoLatitude=79.074456&country=MX'
+};
+
+export const firstLetterUppercase = string => {
+  if (typeof string == 'string') {
+    let returnString = string.split(' ');
+    returnString = returnString.map(stringData => {
+      return (
+        stringData && stringData.replace(/^\w/, c => c.toLocaleUpperCase())
+      );
+    });
+    return returnString.join(' ');
+  }
+  return string;
+};
+
+class CollapsiblePlantProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plantProjectsPickerVisibility: !props.plantProject.name
+    };
+  }
+  togglePicker = () => {
+    this.setState({
+      plantProjectsPickerVisibility: !this.state.plantProjectsPickerVisibility
+    });
+  };
+  render() {
+    const { plantProject, children } = this.props;
+    console.log('__plantProject___', plantProject);
+    return (
+      <div className="pftp-collapsible-card-layout">
+        <div className="pftp-card-header">
+          <div className="project-editor-row">
+            <span className={'plant-project-name'}>
+              {firstLetterUppercase(plantProject.name)}
+            </span>
+            {!this.state.plantProjectsPickerVisibility && (
+              <span>
+                <img
+                  src={EditOrange}
+                  className="icon"
+                  onClick={event => {
+                    this.togglePicker();
+                  }}
+                />
+                <img
+                  src={close_green}
+                  className="icon"
+                  onClick={this.props.deleteProject}
+                />
+              </span>
+            )}
+          </div>
+          {plantProject.name && (
+            <div className="project-basic-info-row">
+              {plantProject.location && (
+                <span className="row">
+                  <img src={MapPinRed} className="small-icon" />
+                  <span>{firstLetterUppercase(plantProject.location)}</span>
+                </span>
+              )}
+              <span className="row">
+                <span className="margin-item light-text">
+                  {plantProject.countPlanted}
+                </span>
+                <span>{'Trees planted'}</span>
+              </span>
+            </div>
+          )}
+        </div>
+        {this.state.plantProjectsPickerVisibility && (
+          <div className="pftp-card-picker">{children}</div>
+        )}
+      </div>
+    );
+  }
+}
+CollapsiblePlantProject.propTypes = {
+  plantProject: PropTypes.object,
+  children: PropTypes.node,
+  deleteProject: PropTypes.func.isRequired
 };
 
 export default class ManageProjects extends Component {
@@ -97,7 +182,14 @@ export default class ManageProjects extends Component {
     let plantProjectList = this.state.plantProjects.map(
       (plantProject, index) => {
         return (
-          <React.Fragment key={index}>
+          <CollapsiblePlantProject
+            key={index}
+            plantProject={plantProject}
+            ref={'CollapsiblePlantProject' + index}
+            deleteProject={() => {
+              this.handleDeleteProjectCLick(plantProject, index);
+            }}
+          >
             <div className="user-profile__project-form-group">
               <div className="plant-project__item">
                 <TCombForm
@@ -109,25 +201,39 @@ export default class ManageProjects extends Component {
                   }}
                   value={plantProject}
                 />
-                <PrimaryButton
-                  onClick={() => {
-                    this.handleSaveProjectClick(plantProject, index);
-                  }}
-                >
-                  {i18n.t('label.save_changes')}
-                </PrimaryButton>
-                <div
-                  key={index}
-                  onClick={() => {
-                    this.handleDeleteProjectCLick(plantProject, index);
-                  }}
-                  className="delete-project"
-                >
-                  {i18n.t('label.delete_project')}
+                <div className="row">
+                  <PrimaryButton
+                    onClick={() => {
+                      this.handleSaveProjectClick(plantProject, index);
+                    }}
+                  >
+                    {i18n.t('label.save_changes')}
+                  </PrimaryButton>
+                  <SecondaryButton
+                    onClick={() => {
+                      if (!plantProject.id) {
+                        //delete this
+                        this.handleDeleteProjectCLick(plantProject, index);
+                      } else {
+                        //collapsed all
+                        console.log('test test');
+                        if (this.refs['CollapsiblePlantProject' + index]) {
+                          this.refs[
+                            'CollapsiblePlantProject' + index
+                          ].togglePicker();
+                        }
+                        // this.setState({
+                        //   plantProjects: _.cloneDeep(this.state.plantProjects)
+                        // });
+                      }
+                    }}
+                  >
+                    {'Discard Changes'}
+                  </SecondaryButton>
                 </div>
               </div>
             </div>
-          </React.Fragment>
+          </CollapsiblePlantProject>
         );
       }
     );
@@ -146,17 +252,21 @@ export default class ManageProjects extends Component {
     return (
       <div className="app-container__content--center sidenav-wrapper edit-user-profile__container ">
         {type == 'tpo' ? (
-          <div className="plant-project__container">
-            {this.getPlantProjectList()}
-            <div className="pftp-addbutton">
-              <button
-                onClick={() => {
-                  this.handleAddNewProject();
-                }}
-              >
-                +&nbsp;{i18n.t('label.new_project')}
-              </button>
+          <div className="manage-project-container">
+            <div className="project-list-container plant-project__container edit-user-profile__container">
+              {this.getPlantProjectList()}
+
+              <div className="pftp-addbutton">
+                <button
+                  onClick={() => {
+                    this.handleAddNewProject();
+                  }}
+                >
+                  +&nbsp;{i18n.t('label.new_project')}
+                </button>
+              </div>
             </div>
+            <div className="donation-info-list" />
           </div>
         ) : null}
       </div>

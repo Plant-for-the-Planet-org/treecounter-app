e976735ac3e51c7e3c971d914d53a093c12a016d
niteshgrg <niteshgarg.06@gmail.com>
2019-04-27 22:43:05 +0530

    FIx date picker android

--- app/components/Templates/DatePickerTemplate.android.js
+++ app/components/Templates/DatePickerTemplate.android.js
@@ -37,11 +37,16 @@ class CollapsibleDatePickerAndroid extends React.PureComponent {
 
   openDatePicker() {
     try {
-      DatePickerAndroid.open({
-        date: new Date(),
-        maxDate: this.props.locals.config.maxDate ? new Date() : null,
-        minDate: this.props.locals.config.minDate ? new Date() : null
-      }).then(date => {
+      let dateObject = {
+        date: new Date()
+      };
+      if (this.props.locals.config.maxDate) {
+        dateObject.maxDate = new Date();
+      }
+      if (this.props.locals.config.minDate) {
+        dateObject.minDate = new Date();
+      }
+      DatePickerAndroid.open(dateObject).then(date => {
         if (date.action !== DatePickerAndroid.dismissedAction) {
           //Please take note that the number of the months is based on
           //the count of an index, let say January is 0, February is 1 and so on...

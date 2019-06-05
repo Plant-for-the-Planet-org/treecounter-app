e9fe0bfc4063edfc92917f61a107570662a8d88b
Norbert Schuler <norbert.schuler@epublica.de>
2019-06-05 15:16:28 +0200

    a few more resources

--- app/components/DownloadAppStore.js
+++ app/components/DownloadAppStore.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 import { AppStoreLogo } from '../assets';
+import i18n from '../../locales/i18n.js';
 
 export class DownloadAppModal extends Component {
   redirectUser() {
     window.location =
-      'https://itunes.apple.com/in/app/plant-for-the-planet/id1444740626?mt=8';
+      'https://itunes.apple.com/app/plant-for-the-planet/id1444740626?mt=8';
   }
   render() {
     let { isOpen, onRequestClose, continueOnSite } = this.props;
@@ -26,9 +27,9 @@ export class DownloadAppModal extends Component {
             className="image_full_width"
             src={AppStoreLogo}
           />
-          <div className="or_class">OR</div>
+          <div className="or_class">{i18n.t('label.OR')}</div>
           <div onClick={continueOnSite} className="continue_on_site">
-            Continue using the site
+            {i18n.t('label.continue_on_site')}
           </div>
         </div>
       </Modal>

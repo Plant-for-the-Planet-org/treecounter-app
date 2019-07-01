bf324c0b28604ace1cea640769e3e7f3df9f8652
yllipetrovci <yllipetrovci@gmail.com>
2019-07-01 21:02:35 +0200

    static keyword for counting months

--- web/widgets/NDVI/app/components/NDVIContainer.js
+++ web/widgets/NDVI/app/components/NDVIContainer.js
@@ -9,6 +9,20 @@ export default class NDVIContainer extends Component {
   render() {
     return (
       <div className="ndvi-container">
+        <div className="row month-keyword">
+          <p>J</p>
+          <p>F</p>
+          <p>M</p>
+          <p>A</p>
+          <p>M</p>
+          <p>J</p>
+          <p>J</p>
+          <p>A</p>
+          <p>S</p>
+          <p>O</p>
+          <p>N</p>
+          <p>D</p>
+        </div>
         <TimeSeries />
         <Legend />
         <GradientProgressbar />

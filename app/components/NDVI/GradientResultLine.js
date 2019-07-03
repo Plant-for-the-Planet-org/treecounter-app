5f8f9df59c5bca217976cc264642961fc9a9f609
yllipetrovci <yllipetrovci@gmail.com>
2019-07-03 14:53:59 +0200

    rename GradientProgressBar to GradientResultLine

---
+++ app/components/NDVI/GradientResultLine.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//style
+
+const GradientResultLine = props => {
+  return (
+    <div className="gradient-result-line-component">
+      <h1>GradientResultLine</h1>
+    </div>
+  );
+};
+
+export default GradientResultLine;

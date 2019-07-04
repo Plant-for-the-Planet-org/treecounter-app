575108791e5228667bab91fd4ff873fc05531f8c
yllipetrovci <yllipetrovci@gmail.com>
2019-07-05 00:39:57 +0200

    refactor parseDate

--- app/components/NDVI/Info.js
+++ app/components/NDVI/Info.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import parseDate from './NDVIfunctions/ParseDate';
+import parseDate from './NDVIfunctions/parseDate';
 
 const Info = props => {
   return (

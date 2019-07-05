f58d06c7acc7d63ce9cf2d2a5de642f119f74db7
yllipetrovci <yllipetrovci@gmail.com>
2019-07-05 20:29:50 +0200

    if there isn't min and max agregate values then return green circle

--- app/components/NDVI/TimeSerieCircle.js
+++ app/components/NDVI/TimeSerieCircle.js
@@ -4,13 +4,19 @@ import getGradiantPosition from './NDVIfunctions/getGradientPosition';
 import colors from './circle-colors';
 
 const TimeSerieCircle = props => {
-  let bgStyle = {
-    backgroundImage: `linear-gradient(to right,
-       ${colors[getGradiantPosition(props.ndviAggregate.min)].color} 0%,
-        ${colors[getGradiantPosition(props.ndviAggregate.max)].color} 100%)`
-  };
+  let bgStyle = {};
+  if (props.ndviAggregate.min && props.ndviAggregate.max) {
+    bgStyle = {
+      backgroundImage: `linear-gradient(to right,
+         ${colors[getGradiantPosition(props.ndviAggregate.min)].color} 0%,
+          ${colors[getGradiantPosition(props.ndviAggregate.max)].color} 100%)`
+    };
+  } else {
+    bgStyle = { background: 'green' };
+  }
 
   const onClick = () => {
+    //notify parent that circle is clicked
     props.onClick();
   };
 

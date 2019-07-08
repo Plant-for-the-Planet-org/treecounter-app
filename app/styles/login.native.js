7b5ca18ef25bf9051783f35fdaabadefeb16ebc3
niteshgrg <niteshgarg.06@gmail.com>
2019-07-08 22:33:08 +0530

    Add padding

--- app/styles/login.native.js
+++ app/styles/login.native.js
@@ -64,7 +64,7 @@ export default (loginStyles = EStyleSheet.create({
   },
   bottomRow: {
     flexDirection: 'row',
-    paddingTop: 10,
+    paddingTop: 15,
     paddingBottom: 10,
     flexWrap: 'wrap',
     justifyContent: 'space-between'

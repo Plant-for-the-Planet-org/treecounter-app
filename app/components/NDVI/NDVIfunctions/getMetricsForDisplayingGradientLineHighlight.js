483322ecee8c1ffe3533d1dd2d71cbb63090a2b8
yllipetrovci <yllipetrovci@gmail.com>
2019-07-13 23:50:25 +0200

    fix language for information component when the browser language it  is deutch

--- app/components/NDVI/NDVIfunctions/getMetricsForDisplayingGradientLineHighlight.js
+++ app/components/NDVI/NDVIfunctions/getMetricsForDisplayingGradientLineHighlight.js
@@ -2,17 +2,14 @@ import getGradiantPosition from './getGradientPosition';
 
 const getMetricsForDisplayingGradientLineHighlight = (min, max) => {
   const gradientLinePercentagePerFraction = 100 / 21;
-  console.log('gradient line:' + gradientLinePercentagePerFraction);
   const minPosition = getGradiantPosition(min); // find the min position in gradient
   const maxPosition = getGradiantPosition(max); // find the max position in gradient
   const intervalFromMinToMax = maxPosition - minPosition + 1; // find the interval including the last last step
 
   const calculateLeftInPercentage =
     minPosition * gradientLinePercentagePerFraction;
-  // const calculateLeftInPercentage = 10;
   const calculateWidthInPercentage =
     intervalFromMinToMax * gradientLinePercentagePerFraction;
-  // calculateLeftInPercentage
   return [calculateLeftInPercentage + '%', calculateWidthInPercentage + '%'];
 };
 

37a4f92ce22e54c36105c4abba1e9503b5a3475f
yllipetrovci <yllipetrovci@gmail.com>
2019-07-18 23:37:32 +0200

    remove unused components

--- app/components/NDVI/GradientResultLine.native.js
+++ app/components/NDVI/GradientResultLine.native.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import getMetricsForDisplayingGradientLineHighlight from './NDVIfunctions/getMetricsForDisplayingGradientLineHighlight';
 import { View, Text, Platform } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import moment from 'moment';

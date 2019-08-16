2f60b7c9a72f35083d1a8af4dd5446cdf3881c77
yllipetrovci <yllipetrovci@gmail.com>
2019-08-16 11:07:29 +0200

    create a call at jorgo api for getting the payment method id

--- app/components/StripePayment/CheckoutForm.js
+++ app/components/StripePayment/CheckoutForm.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { injectStripe } from 'react-stripe-elements';
 import CardSection from './CardSection';
 import PropTypes from 'prop-types';
-// import Axios from 'axios';
+import Axios from 'axios';
 import './stripe.scss';
 import LoadingIndicators from '../../components/Common/LoadingIndicator';
 
 class CheckoutForm extends React.Component {
-  state = { loading: false };
+  state = {
+    loading: false,
+    token:
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NjU3NzM1MDIsImV4cCI6MTU2NTc3NzEwMiwicm9sZXMiOlsiUk9MRV9UUkVFRE9OT1IiLCJST0xFX1VTRVIiXSwidXNlcm5hbWUiOiJ0ZXN0UHJvZEBvbmV6ZXJvZWlnaHQuY28ifQ.qQARDy6FJoW1b9qmhsjJjMmYzUwcfa48-vpfRQxCc2dyTj8yKCdHuJlMlVanwLkMRxfi2i9orS4Jk10oyVpn01kfwnlNtNmKt815x5u9LWYUOMrYW7Vm_zB6J0HGpQYSm3ikbA_5N4D9in8gXZYIYoCFjeZnSRhgmbnBOtDrkcOHKUb52pshsxIJfzSUz3j0d8mhcaQb31gQahIKn7rPJUWyfJm6rVqdQCA2g5zYRUJLZgtNcukIBle-HkgW7jhlFDrHw_b-K8rFcr_Oy6yTXfV6Fh9ZhVMq4NpqYpeufEbab3cu9abo7vXrFPJ-iTXbzDXQgTnmCGKGs2IL6GNINcOMXZgXk1WMXra4_PcSnXvHjku9iM4jplqo1W_u_DGmWWMVs1lhJL16nzhJH5yC9eBF67mkYYtwTglyYLiTpV7V9yuf86bGBgqEj3pZuD49hr2lc0Qq2n7domCGLU6-d8_hJtjN8fuFUgMJnFr9QPuAsTbM0ip7Ehw4qLeic1Be01LG8d9nHqW1wk5czMabtPOwM78dl32wALP-siwgt4hV1niPu0zbC6EKCOm5RNnt2J7UxxF0cxezNRgwexUtkEAuVbD16Uor6iqcPhosrAnwZdebMMBkViPJrqEoOHwINgAHJ123oqfBCXLviFT5Z-Aq093grQG6-iAWS6efXJI'
+  };
   handleSubmit = async ev => {
     // We don't want to let default form submission happen here, which would refresh the page.
     ev.preventDefault();
@@ -37,18 +41,27 @@ class CheckoutForm extends React.Component {
       //   },
       // })
 
-      //just in test case, createSource will be depricated
-      const response = await this.props.stripe.createSource({
-        type: 'ideal',
-        amount: 1099,
-        currency: 'eur',
-        owner: {
-          name: 'Jenny Rosen'
-        },
-        redirect: {
-          return_url: 'https://localhost:8080/stripe'
+      //test case
+      let requestData = {
+        customer: 'cus_FcYGDg4Fxm4SZQ',
+        amount: 2000,
+        currency: 'EUR',
+        payment_method_id: 'src_1F7J9bGhHD5xN1Uq4vJlChre'
+      };
+
+      const requestPaymentIntentUrl =
+        'https://devel.trilliontreecampaign.org/app_dev.php/public/v1.3/stripe/paymentIntent/request';
+      let config = {
+        headers: {
+          Authorization: 'Bearer ' + this.state.token
         }
-      });
+      };
+
+      const response = await Axios.post(
+        requestPaymentIntentUrl,
+        requestData,
+        config
+      );
 
       console.log(response);
       if (response.source.id !== undefined) {

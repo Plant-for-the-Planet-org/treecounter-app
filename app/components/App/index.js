da04e30d33366044c1b03ac3a741d399c8f013e0
Mustak Ahmed <mahmed0715@gmail.com>
2019-08-19 00:40:27 +0600

    New App banner implementation

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux';
 import TreeCounter from './TreeCounter';
 import configureStore from '../../stores/TreecounterStore';
 import GlobalErrorBoundary from '../ErrorBoundry/globalErrorBoundry';
-
+import { isIOS, isAndroid } from '../../utils/utils';
+import SmartBanner from '../SmartBanner';
+import i18n from '../../locales/i18n.js';
 let store;
 
 export default class App extends Component {
@@ -18,6 +20,14 @@ export default class App extends Component {
       <Provider store={store}>
         <GlobalErrorBoundary>
           <TreeCounter />
+          <SmartBanner
+            daysHidden={1}
+            title={i18n.t('label.Plant_for_the_planet_App')}
+            author={i18n.t('label.A_solution_for_Climate_Crisis')}
+            button={i18n.t('label.getAppButtonText')}
+            force={isIOS() ? 'ios' : isAndroid() ? 'android' : ''}
+            position={'bottom'}
+          />
         </GlobalErrorBoundary>
       </Provider>
     );
--- app/utils/utils.js
+++ app/utils/utils.js
@@ -7,3 +7,31 @@ export function delimitNumbers(str) {
     );
   });
 }
+
+function getMobileOperatingSystem() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return 'Windows Phone';
+  }
+
+  if (/android/i.test(userAgent)) {
+    return 'Android';
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return 'iOS';
+  }
+
+  return 'unknown';
+}
+
+export function isIOS() {
+  return getMobileOperatingSystem() == 'iOS';
+}
+
+export function isAndroid() {
+  return getMobileOperatingSystem() == 'Android';
+}

2b5c4b710e93979965fd480cb7168eec183ffd98
Hkurra <er.harshkurra@gmail.com>
2019-08-21 13:55:42 +0530

    Fixed bug #448 : Added react-datepicker library and create custom Template with custom CSS See https://github.com/Hacker0x01/react-datepicker for more info

--- app/components/Templates/DatePickerTemplate.js
+++ app/components/Templates/DatePickerTemplate.js
@@ -1,1 +1,44 @@
-export { TextInputTemplate as DatePickerTemplate } from './TextInputTemplate';
+import React from 'react';
+import i18n from '../../locales/i18n';
+import { formatDate, getDateFromMySQL } from '../../helpers/utils';
+import { getLocale } from '../../actions/getLocale';
+// import { compCalendar } from '../../assets';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+export function DatePickerTemplate(locals) {
+  const locale = getLocale();
+  let error = locals.hasError;
+  const containerStyle = { width: '100%' };
+  const calendarStyle = { marginBottom: 20 };
+  return locals.type !== 'hidden' ? (
+    <div
+      style={containerStyle}
+      className={'pftp-date-picker__container'}
+      tabIndex="0"
+    >
+      {/* <label className={locals.value !== '' ? 'float-label' : ''}>
+        {i18n.t(locals.label)}
+      </label> */}
+      <DatePicker
+        placeholderText={i18n.t(locals.label)}
+        style={calendarStyle}
+        openDatePickerOnClick={true}
+        selected={!!locals.value ? getDateFromMySQL(locals.value) : undefined}
+        // startDate={!!locals.value ? getDateFromMySQL(locals.value) : undefined}
+        // startLimit={new Date(1700, 0)}
+        // endLimit={new Date()}
+        locale={locale}
+        key={101}
+        onChange={newDate => {
+          console.log('___onChangeDate___', newDate);
+
+          locals.onChange(formatDate(newDate));
+        }}
+      />
+
+      {locals.error ? locals.error : null}
+    </div>
+  ) : null;
+}

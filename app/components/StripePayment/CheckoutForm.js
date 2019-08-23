aa6338695a6a3c084114acfca02674f877797f6b
yllipetrovci <yllipetrovci@gmail.com>
2019-08-23 14:27:02 +0200

    Add component for SEPA payment

--- app/components/StripePayment/CheckoutForm.js
+++ app/components/StripePayment/CheckoutForm.js
@@ -46,12 +46,11 @@ class CheckoutForm extends React.Component {
       'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NjU3NzM1MDIsImV4cCI6MTU2NTc3NzEwMiwicm9sZXMiOlsiUk9MRV9UUkVFRE9OT1IiLCJST0xFX1VTRVIiXSwidXNlcm5hbWUiOiJ0ZXN0UHJvZEBvbmV6ZXJvZWlnaHQuY28ifQ.qQARDy6FJoW1b9qmhsjJjMmYzUwcfa48-vpfRQxCc2dyTj8yKCdHuJlMlVanwLkMRxfi2i9orS4Jk10oyVpn01kfwnlNtNmKt815x5u9LWYUOMrYW7Vm_zB6J0HGpQYSm3ikbA_5N4D9in8gXZYIYoCFjeZnSRhgmbnBOtDrkcOHKUb52pshsxIJfzSUz3j0d8mhcaQb31gQahIKn7rPJUWyfJm6rVqdQCA2g5zYRUJLZgtNcukIBle-HkgW7jhlFDrHw_b-K8rFcr_Oy6yTXfV6Fh9ZhVMq4NpqYpeufEbab3cu9abo7vXrFPJ-iTXbzDXQgTnmCGKGs2IL6GNINcOMXZgXk1WMXra4_PcSnXvHjku9iM4jplqo1W_u_DGmWWMVs1lhJL16nzhJH5yC9eBF67mkYYtwTglyYLiTpV7V9yuf86bGBgqEj3pZuD49hr2lc0Qq2n7domCGLU6-d8_hJtjN8fuFUgMJnFr9QPuAsTbM0ip7Ehw4qLeic1Be01LG8d9nHqW1wk5czMabtPOwM78dl32wALP-siwgt4hV1niPu0zbC6EKCOm5RNnt2J7UxxF0cxezNRgwexUtkEAuVbD16Uor6iqcPhosrAnwZdebMMBkViPJrqEoOHwINgAHJ123oqfBCXLviFT5Z-Aq093grQG6-iAWS6efXJI'
   };
 
-  componentDidMount() {
-    console.log('HEJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ');
-    console.log(this.props);
-  }
+  handleSubmitSEPAPayment = async ev => {
+    console.log('SEPA PAYMENT SUBMITED');
+  };
 
-  handleSubmit = async ev => {
+  handleSubmitCCPayment = async ev => {
     //available attributes
     // currency, donorAddress, donorZipCode, donorCity, donorCountry, donorEmail, donorName, treeCount: this.state.selectedTreeCount,
 
@@ -132,15 +131,23 @@ class CheckoutForm extends React.Component {
   };
 
   render() {
+    const props = this.props;
     let arrow = classnames({
-      arrow: !this.props.expanded
+      arrow: !props.expanded
     });
     let displayNone = classnames({
-      'display-none': !this.props.expanded
+      'display-none': !props.expanded
     });
-    const paymentType = this.props.paymentType;
+    const paymentType = props.paymentType;
     return !this.state.loading ? (
-      <form className="payment-option" onSubmit={this.handleSubmit}>
+      <form
+        className="payment-option"
+        onSubmit={
+          paymentType === 'stripe_cc'
+            ? this.handleSubmitCCPayment
+            : this.handleSubmitSEPAPayment
+        }
+      >
         <div onClick={this.handleArrowClick} className="payment-option-header">
           <span>
             <img

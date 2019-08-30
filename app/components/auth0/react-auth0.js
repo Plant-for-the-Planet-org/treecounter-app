3f5c461a0cb6b0b115b69d3d330e506d32045508
Chris Sattinger <crucialfelix@gmail.com>
2019-08-30 10:38:33 +0200

    Add react-auth0 with AuthProvider, useAuth()

---
+++ app/components/auth0/react-auth0.js
@@ -0,0 +1,129 @@
+import createAuth0Client from '@auth0/auth0-spa-js';
+import PropTypes from 'prop-types';
+import React, { useContext, useEffect, useState } from 'react';
+import history from '../../utils/history';
+
+// const DEFAULT_REDIRECT_CALLBACK = () =>
+//   window.history.replaceState({}, document.title, window.location.pathname);
+
+/**
+ * On authentication (login/logout), redirect to a targetUrl
+ * or reload the current page.
+ * This does not use the router because the app should be forced to reload.
+ */
+const DEFAULT_REDIRECT_CALLBACK = appState => {
+  // TODO if a PrivateRoute forces authorization
+  // then it should return to that URL, but instead it goes
+  // to the URL you were on before you clicked on the private one.
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+export const Auth0Context = React.createContext();
+
+/**
+ * Provides user authentication variables and functions
+ *
+ *  isAuthenticated,
+ *  user,
+ *  loading,
+ *  popupOpen(),
+ *  loginWithPopup(),
+ *  handleRedirectCallback(),
+ *  getIdTokenClaims()
+ *  loginWithRedirect()
+ *  getTokenSilently()
+ *  getTokenWithPopup()
+ *  logout()
+ */
+export const useAuth0 = () => useContext(Auth0Context);
+
+/**
+ * Context provider that is wrapped around the entire application,
+ * so any functional component can get access to user and auth state.
+ */
+export const Auth0Provider = ({
+  children,
+  onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
+  ...initOptions
+}) => {
+  const [isAuthenticated, setIsAuthenticated] = useState();
+  const [user, setUser] = useState();
+  const [auth0Client, setAuth0] = useState();
+  const [loading, setLoading] = useState(true);
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  useEffect(() => {
+    const initAuth0 = async () => {
+      const auth0FromHook = await createAuth0Client(initOptions);
+      setAuth0(auth0FromHook);
+
+      if (window.location.search.includes('code=')) {
+        const { appState } = await auth0FromHook.handleRedirectCallback();
+        onRedirectCallback(appState);
+      }
+
+      const isAuthenticated = await auth0FromHook.isAuthenticated();
+
+      setIsAuthenticated(isAuthenticated);
+
+      if (isAuthenticated) {
+        const user = await auth0FromHook.getUser();
+        setUser(user);
+      }
+
+      setLoading(false);
+    };
+    initAuth0();
+  }, []);
+
+  const loginWithPopup = async (params = {}) => {
+    setPopupOpen(true);
+    try {
+      await auth0Client.loginWithPopup(params);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setPopupOpen(false);
+    }
+    const user = await auth0Client.getUser();
+    setUser(user);
+    setIsAuthenticated(true);
+  };
+
+  const handleRedirectCallback = async () => {
+    setLoading(true);
+    await auth0Client.handleRedirectCallback();
+    const user = await auth0Client.getUser();
+    setLoading(false);
+    setIsAuthenticated(true);
+    setUser(user);
+  };
+  return (
+    <Auth0Context.Provider
+      value={{
+        isAuthenticated,
+        user,
+        loading,
+        popupOpen,
+        loginWithPopup,
+        handleRedirectCallback,
+        getIdTokenClaims: (...p) => auth0Client.getIdTokenClaims(...p),
+        loginWithRedirect: (...p) => auth0Client.loginWithRedirect(...p),
+        getTokenSilently: (...p) => auth0Client.getTokenSilently(...p),
+        getTokenWithPopup: (...p) => auth0Client.getTokenWithPopup(...p),
+        logout: (...p) => auth0Client.logout(...p)
+      }}
+    >
+      {children}
+    </Auth0Context.Provider>
+  );
+};
+
+Auth0Provider.propTypes = {
+  children: PropTypes.any,
+  onRedirectCallback: PropTypes.func
+};
---
+++ app/utils/history.js
@@ -0,0 +1,4 @@
+// Manage session history with JavaScript
+// history is a dependency of react-router
+import { createBrowserHistory } from 'history';
+export default createBrowserHistory();

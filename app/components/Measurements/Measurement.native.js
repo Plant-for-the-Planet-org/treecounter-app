74887a0074498c54819b4f27cf7ddf68193c3d70
Hkurra <er.harshkurra@gmail.com>
2019-09-13 12:08:56 +0530

    removed unwanted comment

--- app/components/Measurements/Measurement.native.js
+++ app/components/Measurements/Measurement.native.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
-// { plantedDate: 'On April 9,2019', heightOfTree: 150, widhtOfTree: 120 },
 import ArrowUpIcon from '../../assets/images/green-arrow-up.png';
 import ArrowRightIcon from '../../assets/images/green-arrow-right.png';
 import CalendarIcon from '../../assets/images/green-calendar.png';

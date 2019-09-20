5cddbaf020ac98b5b0bf9d45b10d0ace7874933c
Mustak Ahmed <mahmed0715@gmail.com>
2019-09-20 17:11:23 +0600

    eslint fix: no target blank

--- app/components/FAQ/index.js
+++ app/components/FAQ/index.js
@@ -44,7 +44,11 @@ export default class FAQ extends Component {
           {i18n.t('label.faqs')}
           <DescriptionHeading>
             {i18n.t('label.description1')}
-            <a href={'https://' + i18n.t('label.description2')} target="_blank">
+            <a
+              href={'https://' + i18n.t('label.description2')}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {i18n.t('label.description2')}
             </a>{' '}
             {i18n.t('label.description3')}

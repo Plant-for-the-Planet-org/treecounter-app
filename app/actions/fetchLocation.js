22be3dc7b5f0265db047ac98fdccc9e66c989af7
Mustak Ahmed <mahmed0715@gmail.com>
2019-10-28 11:52:16 +0600

    Update code to fetch currency

--- app/actions/fetchLocation.js
+++ app/actions/fetchLocation.js
@@ -4,11 +4,17 @@ import countryCodes from '../assets/countryCodes.json';
 import supportedCurrency from '../assets/supportedCurrency.json';
 import { find } from 'lodash';
 import { setCurrencyAction } from './globalCurrency';
+import { context } from '../config';
 
 export function fetchLocation() {
+  // fetchCurrency();
   return dispatch => {
     if (!getItemSync('preferredCurrency')) {
-      getExternalRequest()
+      getExternalRequest({
+        endPoint: `http://api.ipstack.com/check?access_key=${
+          context.locationApikKey
+        }&fields=location,country_code,currency`
+      })
         .then(data => {
           console.log('Got location fetch ip', data);
           const foundLocation = find(countryCodes, {
@@ -22,5 +28,23 @@ export function fetchLocation() {
           console.error(error);
         });
     }
+  };
+}
+
+export function fetchCurrency() {
+  return dispatch => {
+    getExternalRequest({
+      endPoint: `hhttps://openexchangerates.org/api/latest.json?app_id=${
+        context.currencyApiKey
+      }`
+    })
+      .then(data => {
+        console.log('Got currency fetch ip', data);
+        //                dispatch(setCurrencyAction(foundLocation.code));
+      })
+
+      .catch(error => {
+        console.error(error);
+      });
   };
 }

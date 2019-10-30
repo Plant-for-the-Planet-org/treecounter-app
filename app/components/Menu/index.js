3ef56ee1addbafd5bade5658d6d0b987934b40bf
Mustak Ahmed <mahmed0715@gmail.com>
2019-10-30 14:59:27 +0600

    Fix language currency selector invisibl eissue on safari

--- app/components/Menu/index.js
+++ app/components/Menu/index.js
@@ -180,7 +180,7 @@ class Menu extends Component {
   render() {
     let { path, pathname } = this.props;
 
-    return (
+    return [
       <div
         className={
           'app-container__sidenav ' + (this.props.isOpen ? 'open' : '')
@@ -251,51 +251,50 @@ class Menu extends Component {
         ))}
 
         {this.props.userProfile ? this.renderShareButtons() : null}
-
-        <div className="badge-wrapper">
-          <div className="global-selector language">
-            <div className="li-select">
-              <Select
-                defaultValue={this.state.selectedLanguage}
-                value={this.state.selectedLanguage}
-                options={statusOptions}
-                styles={customStyles}
-                menuPlacement="top"
-                onChange={this.onSelectLanguageChange}
-                isSearchable={false}
-                components={{ Option: IconOption, SingleValue: singleValue }}
-              />
-            </div>
-          </div>
-          <GlobalCurrencySelector
-            userProfile={this.props.userProfile}
-            updateUserProfile={this.props.updateUserProfile}
-          />
-          <div className="global-selector">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={`https://apps.apple.com/app/plant-for-the-planet/id${
-                context['ios'].appId
-              }`}
-            >
-              <img src={images['appleStoreBadge_' + userLang]} />
-            </a>
-          </div>
-          <div className="global-selector">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={`https://play.google.com/store/apps/details?id=${
-                context['android'].appId
-              }`}
-            >
-              <img src={images['googlePlayBadge_' + userLang]} />
-            </a>
+      </div>,
+      <div className="badge-wrapper">
+        <div className="global-selector language">
+          <div className="li-select">
+            <Select
+              defaultValue={this.state.selectedLanguage}
+              value={this.state.selectedLanguage}
+              options={statusOptions}
+              styles={customStyles}
+              menuPlacement="top"
+              onChange={this.onSelectLanguageChange}
+              isSearchable={false}
+              components={{ Option: IconOption, SingleValue: singleValue }}
+            />
           </div>
         </div>
+        <GlobalCurrencySelector
+          userProfile={this.props.userProfile}
+          updateUserProfile={this.props.updateUserProfile}
+        />
+        <div className="global-selector">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://apps.apple.com/app/plant-for-the-planet/id${
+              context['ios'].appId
+            }`}
+          >
+            <img src={images['appleStoreBadge_' + userLang]} />
+          </a>
+        </div>
+        <div className="global-selector">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://play.google.com/store/apps/details?id=${
+              context['android'].appId
+            }`}
+          >
+            <img src={images['googlePlayBadge_' + userLang]} />
+          </a>
+        </div>
       </div>
-    );
+    ];
   }
 }
 const mapDispatchToProps = dispatch => {

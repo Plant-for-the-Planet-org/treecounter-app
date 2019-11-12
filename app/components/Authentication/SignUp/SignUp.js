479a63e193f8ff6093798924fe8cca484397f13e
haideralishah <headeralishah@gmail.com>
2019-11-12 19:17:37 +0500

    SignUp - Code Spliting with React Lazy - [Update]

--- app/components/Authentication/SignUp/SignUp.js
+++ app/components/Authentication/SignUp/SignUp.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import PropTypes from 'prop-types';
 import t from 'tcomb-form';
 
-import PrimaryButton from '../../Common/Button/PrimaryButton';
-import TextHeading from '../../Common/Heading/TextHeading';
-import CardLayout from '../../Common/Card';
-import SignUpType from './SignUpType';
+const PrimaryButton = lazy(() => import('../../Common/Button/PrimaryButton'));
+const TextHeading = lazy(() => import('../../Common/Heading/TextHeading'));
+const CardLayout = lazy(() => import('../../Common/Card'));
+const SignUpType = lazy(() => import('./SignUpType'));
+
 import { SignupJustMe, SignupOrganization } from '../../../assets';
 import { signupFormSchema } from '../../../server/parsedSchemas/signup';
 import i18n from '../../../locales/i18n.js';

9ca792bc2f060686071136e644836c48f7772677
Harsh Vitra <harshvitra@gmail.com>
2019-11-19 19:40:27 +0530

    Home page, on Logout Events wont disappear

--- app/components/TreecounterGraphics/Trillion.js
+++ app/components/TreecounterGraphics/Trillion.js
@@ -12,6 +12,8 @@ import { getImageUrl } from '../../actions/apiRouting';
 import { pledgeEventSelector } from '../../selectors';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { fetchpledgeEventsAction } from '../../actions/pledgeEventsAction';
+import { bindActionCreators } from 'redux';
 
 class Trillion extends Component {
   constructor() {
@@ -25,6 +27,7 @@ class Trillion extends Component {
   }
 
   componentDidMount() {
+    this.props.fetchpledgeEventsAction();
     trillionCampaign()
       .then(({ data }) => {
         this.setState({
@@ -40,6 +43,16 @@ class Trillion extends Component {
         });
       })
       .catch(error => console.log(error));
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.pledgeEvents &&
+      this.props.pledgeEvents &&
+      prevProps.pledgeEvents !== this.props.pledgeEvents
+    ) {
+      this.props.fetchpledgeEventsAction();
+    }
   }
 
   shouldComponentUpdate() {
@@ -111,8 +124,13 @@ class Trillion extends Component {
 const mapStateToProps = state => ({
   pledgeEvents: pledgeEventSelector(state)
 });
-export default connect(mapStateToProps)(Trillion);
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ fetchpledgeEventsAction }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Trillion);
 
 Trillion.propTypes = {
-  pledgeEvents: PropTypes.object
+  pledgeEvents: PropTypes.object,
+  fetchpledgeEventsAction: PropTypes.func
 };

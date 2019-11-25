c233bc4cc2e4db8fbdb9ebf66ae03b2f74193478
sanjay radadiya <sanjay.radadiya91@gmail.com>
2019-11-25 20:46:18 +0530

    change the app code as per react native document
    https://react-native-community.github.io/upgrade-helper/?from=0.59.8&to=0.61.4

--- android/app/src/main/java/org/pftp/MainApplication.java
+++ android/app/src/main/java/org/pftp/MainApplication.java
@@ -3,6 +3,8 @@ package org.pftp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import android.content.Context;
+import com.facebook.react.PackageList;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 //import com.taessina.paypal.RNPaypalWrapperPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -16,11 +18,12 @@ import com.horcrux.svg.SvgPackage;
 
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+// import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
 // import com.reactlibrary.RNTooltipsPackage;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -33,20 +36,16 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new AsyncStoragePackage(),
-            //new RNPaypalWrapperPackage(),
-            new LinearGradientPackage(),
-            BugsnagReactNative.getPackage(),
-            new ReactNativeYouTube(),
-           // new StripeReactPackage(),
-            new RNDeviceInfo(),
-            new ImagePickerPackage(),
-            new RNI18nPackage(),
-            new SvgPackage()
-          //  new RNTooltipsPackage()
-      );
+      
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
     }
 
     @Override
@@ -64,5 +63,32 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+  }
+}
+
+/**
+* Loads Flipper in React Native templates.
+*
+* @param context
+*/
+private static void initializeFlipper(Context context) {
+  if (BuildConfig.DEBUG) {
+    try {
+      /*
+        We use reflection here to pick up the class that initializes Flipper,
+      since Flipper library is not available in release mode
+      */
+      Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+      aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
   }
 }

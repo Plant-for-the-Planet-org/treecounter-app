7f9076825c7deab812c54b1fcc50412fa6fa1601
haideralishah <headeralishah@gmail.com>
2019-11-28 17:11:36 +0500

    ContentLoader.native - Modified Content Loader in native file - [UPDATE]

--- app/components/Common/ContentLoader.native.js
+++ app/components/Common/ContentLoader.native.js
@@ -4,137 +4,220 @@ import ContentLoader from 'react-native-content-loader';
 import { Circle, Rect } from 'react-native-svg';
 
 const AppHomeLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Circle cx="59" cy="75" r="35" />
-        <Rect x="110" y="45" rx="0" ry="0" width="160" height="15" />
-        <Rect x="110" y="70" rx="0" ry="0" width="130" height="15" />
-        <Rect x="110" y="95" rx="0" ry="0" width="130" height="15" />
-        <Circle cx="185" cy="325" r="110" />
-        <Rect x="24" y="525" rx="0" ry="0" width="320" height="13" />
-        <Rect x="24" y="545" rx="0" ry="0" width="290" height="13" />
-        <Rect x="24" y="565" rx="0" ry="0" width="320" height="13" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 const CompetitionLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 const LeaderBoardLoader = () => (
-    <ContentLoader height={300} duration={1000}>
-        <Rect x="0" y="30" rx="0" ry="0" width="100" height="13" />
-        <Rect x="0" y="55" rx="5" ry="5" width="200" height="26" />
-        <Circle cx="250" cy="60" r="30" />
-    </ContentLoader>
+  <ContentLoader height={300} duration={1000}>
+    <Rect x="0" y="30" rx="0" ry="0" width="100" height="13" />
+    <Rect x="0" y="55" rx="5" ry="5" width="200" height="26" />
+    <Circle cx="250" cy="60" r="30" />
+  </ContentLoader>
 );
 const WorldLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-    <Circle cx="59" cy="75" r="35" />
-    <Rect x="110" y="45" rx="0" ry="0" width="160" height="15" />
-    <Rect x="110" y="70" rx="0" ry="0" width="130" height="15" />
-    <Rect x="110" y="95" rx="0" ry="0" width="130" height="15" />
-    <Circle cx="185" cy="325" r="110" />
-    <Rect x="24" y="525" rx="0" ry="0" width="320" height="13" />
-    <Rect x="24" y="545" rx="0" ry="0" width="290" height="13" />
-    <Rect x="24" y="565" rx="0" ry="0" width="320" height="13" />
-</ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 const ProfileLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 const PublicProfileLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 
 const CompetitionListLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 
 const CompetitionSingleLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 
 const ProjectListLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 
 const ProjectSingleLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 
 const DefaultLoader = () => (
-    <ContentLoader height={740} duration={1000}>
-        <Rect x="0" y="10" rx="0" ry="0" width="300" height="160" />
-        <Rect x="0" y="190" rx="0" ry="0" width="80" height="10" />
-        <Rect x="0" y="220" rx="0" ry="0" width="120" height="7" />
-        <Rect x="0" y="250" rx="0" ry="0" width="120" height="10" />
-        <Rect x="150" y="250" rx="0" ry="0" width="120" height="10" />
-    </ContentLoader>
+  <ContentLoader
+    height={980}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="215" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="240" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="270" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
 );
 
-
 const ContentLoading = props => {
-    const { screen } = props;
-    return (
-        <View style={[loadingIndicatorStyle]}>
-            {screen === 'AppHome' && <WorldLoader />}
-            {screen === 'LeaderBoard' && <LeaderBoardLoader />}
-            {screen === 'Competition' && <CompetitionLoader />}
-            {screen === 'worldLoader' && <WorldLoader />}
-            {screen === 'profileLoader' && <ProfileLoader />}
-            {screen === 'publicProfileLoader' && <PublicProfileLoader />}
-            {screen === 'competitionListLoader' && <CompetitionListLoader />}
-            {screen === 'competitionSingleLoader' && <CompetitionSingleLoader />}
-            {screen === 'projectListLoader' && <ProjectListLoader />}
-            {screen === 'projectSingleLoader' && <ProjectSingleLoader />}
-            {screen === 'defaultLoader' && <DefaultLoader />}
-        </View>
-    );
+  const { screen } = props;
+  return (
+    <View style={[loadingIndicatorStyle]}>
+      {screen === 'AppHome' && <WorldLoader />}
+      {screen === 'LeaderBoard' && <LeaderBoardLoader />}
+      {screen === 'Competition' && <CompetitionLoader />}
+      {screen === 'worldLoader' && <WorldLoader />}
+      {screen === 'profileLoader' && <ProfileLoader />}
+      {screen === 'publicProfileLoader' && <PublicProfileLoader />}
+      {screen === 'competitionListLoader' && <CompetitionListLoader />}
+      {screen === 'competitionSingleLoader' && <CompetitionSingleLoader />}
+      {screen === 'projectListLoader' && <ProjectListLoader />}
+      {screen === 'projectSingleLoader' && <ProjectSingleLoader />}
+      {screen === 'defaultLoader' && <DefaultLoader />}
+    </View>
+  );
 };
 const loadingIndicatorStyle = {
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'column',
-    marginTop: '5%'
+  flex: 1,
+  alignItems: 'center',
+  marginTop: '5%'
+  // justifyContent: "center"
 };
 export default ContentLoading;

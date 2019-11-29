52c4e5743e30276193207979e4857505edac4db3
sanjay radadiya <sanjay.radadiya91@gmail.com>
2019-11-29 14:11:52 +0530

    fixed packages issue for android

--- android/app/src/main/java/org/pftp/MainApplication.java
+++ android/app/src/main/java/org/pftp/MainApplication.java
@@ -3,6 +3,14 @@ package org.pftp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.imagepicker.ImagePickerPackage;
+import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 //import com.taessina.paypal.RNPaypalWrapperPackage;
 //import com.gettipsi.stripe.StripeReactPackage;
@@ -28,7 +36,15 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new ReactNativeYouTube(),
+            new SvgPackage(),
+            new RNDeviceInfo(),
+            new ImagePickerPackage(),
+            new RNPermissionsPackage(),
+            new RNI18nPackage(),
+            new AsyncStoragePackage(),
+            new ReanimatedPackage(),
+            new RNGestureHandlerPackage(),
+            new ReactNativeYouTube()
             //new RNPaypalWrapperPackage(),
            // new StripeReactPackage()
           //  new RNTooltipsPackage()

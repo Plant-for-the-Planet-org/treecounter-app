4364a710f37f029b1606f0df904ec08e25c042fe
haideralishah <headeralishah@gmail.com>
2019-11-30 13:40:44 +0500

    add react-native-vector-icon in android/app/build.gradle & mainapplication.java - [UPDATE]

--- android/app/src/main/java/org/pftp/MainApplication.java
+++ android/app/src/main/java/org/pftp/MainApplication.java
@@ -3,6 +3,7 @@ package org.pftp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 //import com.taessina.paypal.RNPaypalWrapperPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new VectorIconsPackage(),
             new AsyncStoragePackage(),
             //new RNPaypalWrapperPackage(),
             new LinearGradientPackage(),

7baa0a627d00db75ab472e11ed8d191515b975d1
Norbert Schuler <norbert.schuler@epublica.de>
2019-12-02 15:35:56 +0100

    upgrade Android project for react-native 0.6x

--- android/app/src/main/java/org/pftp/MainActivity.java
+++ android/app/src/main/java/org/pftp/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -15,13 +16,13 @@ public class MainActivity extends ReactActivity {
         return "TreecounterApp";
     }
 
-   @Override
-   protected ReactActivityDelegate createReactActivityDelegate() {
-     return new ReactActivityDelegate(this, getMainComponentName()) {
-       @Override
-       protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-       }
-     };
-   }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
 }

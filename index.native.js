c5d8209161c853b516c20b7f991db38a095457f6
haideralishah <headeralishah@gmail.com>
2019-12-06 13:59:08 +0500

    UI fixed - [NONE]

--- index.native.js
+++ index.native.js
@@ -1,10 +1,9 @@
 import { AppRegistry } from 'react-native';
 import App from './app/components/App';
-import WelcomScreenSlider from './app/components/Welcome/WelcomeSlider';
 import './ReactotronConfig';
 /* app.js */
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
 
-AppRegistry.registerComponent('TreecounterApp', () => WelcomScreenSlider);
+AppRegistry.registerComponent('TreecounterApp', () => App);

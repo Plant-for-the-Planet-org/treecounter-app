c81330f2f2d07041a1bb8a3e4f77b2767ec2a023
Mustak Ahmed <mahmed0715@gmail.com>
2019-12-11 23:39:58 +0600

    show button as disabled

--- app/components/GiftTrees/Tabs/FormikFormGift.native.js
+++ app/components/GiftTrees/Tabs/FormikFormGift.native.js
@@ -9,6 +9,7 @@ import { generateFormikSchemaFromFormSchema } from '../../../helpers/utils';
 import buttonStyles from '../../../styles/common/button.native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import giftInvitationFormSchema from '../../../server/formSchemas/giftTrees';
+import { opaqueType } from '@babel/types';
 
 export default class FormikFormGift extends Component {
   constructor(props) {
@@ -57,33 +58,75 @@ export default class FormikFormGift extends Component {
         }}
         validationSchema={validationSchema}
       >
-        {props => (
-          <>
-            <View style={styles.view_container}>
-              <View style={{ padding: 20, paddingBottom: 0 }}>
-                <Text style={styles.add_competition_title}>
-                  {i18n.t('label.gift_receipient')}
-                </Text>
-                <Text style={{ fontSize: 16, color: '#4d5153' }}>
-                  {i18n.t('label.gift_trees_description')}
-                </Text>
-              </View>
-              <KeyboardAwareScrollView
-                contentContainerStyle={[
-                  styles.formScrollView,
-                  { paddingTop: 0 }
-                ]}
-                enableOnAndroid
-                keyboardDismissMode="on-drag"
-                keyboardShouldPersistTaps="always"
-                resetScrollToCoords={{ x: 0, y: 0 }}
-                scrollEnabled
-              >
-                <View style={styles.formView}>
-                  <View style={styles.formHalfTextField}>
+        {props => {
+          let isValid = props;
+          let opacityValue = isValid ? 1 : '0.2';
+          return (
+            <>
+              {console.log('props', props)}
+              <View style={styles.view_container}>
+                <View style={{ padding: 20, paddingBottom: 0 }}>
+                  <Text style={styles.add_competition_title}>
+                    {i18n.t('label.gift_receipient')}
+                  </Text>
+                  <Text style={{ fontSize: 16, color: '#4d5153' }}>
+                    {i18n.t('label.gift_trees_description')}
+                  </Text>
+                </View>
+                <KeyboardAwareScrollView
+                  contentContainerStyle={[
+                    styles.formScrollView,
+                    { paddingTop: 0 }
+                  ]}
+                  enableOnAndroid
+                  keyboardDismissMode="on-drag"
+                  keyboardShouldPersistTaps="always"
+                  resetScrollToCoords={{ x: 0, y: 0 }}
+                  scrollEnabled
+                >
+                  <View style={styles.formView}>
+                    <View style={styles.formHalfTextField}>
+                      <TextField
+                        label={i18n.t('label.First_Name')}
+                        value={props.values.firstname}
+                        tintColor={'#89b53a'}
+                        titleFontSize={12}
+                        returnKeyType="next"
+                        lineWidth={1}
+                        labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        titleTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        blurOnSubmit={false}
+                        error={
+                          props.touched.firstname && props.errors.firstname
+                        }
+                        onChangeText={props.handleChange('firstname')}
+                        onBlur={props.handleBlur('firstname')}
+                      />
+                    </View>
+
+                    <View style={styles.formHalfTextField}>
+                      <TextField
+                        label={i18n.t('label.Last_Name')}
+                        value={props.values.lastname}
+                        tintColor={'#89b53a'}
+                        titleFontSize={12}
+                        returnKeyType="next"
+                        lineWidth={1}
+                        labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        titleTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        blurOnSubmit={false}
+                        error={props.touched.lastname && props.errors.lastname}
+                        onChangeText={props.handleChange('lastname')}
+                        onBlur={props.handleBlur('lastname')}
+                      />
+                    </View>
+                  </View>
+                  <View>
                     <TextField
-                      label={i18n.t('label.First_Name')}
-                      value={props.values.firstname}
+                      label={i18n.t('label.Email')}
+                      value={props.values.email}
                       tintColor={'#89b53a'}
                       titleFontSize={12}
                       returnKeyType="next"
@@ -92,103 +135,78 @@ export default class FormikFormGift extends Component {
                       titleTextStyle={{ fontFamily: 'OpenSans-Regular' }}
                       affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
                       blurOnSubmit={false}
-                      error={props.touched.firstname && props.errors.firstname}
-                      onChangeText={props.handleChange('firstname')}
-                      onBlur={props.handleBlur('firstname')}
+                      error={props.touched.email && props.errors.email}
+                      onChangeText={props.handleChange('email')}
+                      onBlur={props.handleBlur('email')}
                     />
                   </View>
-
-                  <View style={styles.formHalfTextField}>
+                  <View>
                     <TextField
-                      label={i18n.t('label.Last_Name')}
-                      value={props.values.lastname}
+                      label={i18n.t('label.Gift_Message')}
+                      value={props.values.message}
                       tintColor={'#89b53a'}
                       titleFontSize={12}
                       returnKeyType="next"
                       lineWidth={1}
+                      multiline
                       labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
                       titleTextStyle={{ fontFamily: 'OpenSans-Regular' }}
                       affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
                       blurOnSubmit={false}
-                      error={props.touched.lastname && props.errors.lastname}
-                      onChangeText={props.handleChange('lastname')}
-                      onBlur={props.handleBlur('lastname')}
+                      error={props.touched.message && props.errors.message}
+                      onChangeText={props.handleChange('message')}
+                      onBlur={props.handleBlur('message')}
                     />
                   </View>
-                </View>
-                <View>
-                  <TextField
-                    label={i18n.t('label.Email')}
-                    value={props.values.email}
-                    tintColor={'#89b53a'}
-                    titleFontSize={12}
-                    returnKeyType="next"
-                    lineWidth={1}
-                    labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    titleTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    blurOnSubmit={false}
-                    error={props.touched.email && props.errors.email}
-                    onChangeText={props.handleChange('email')}
-                    onBlur={props.handleBlur('email')}
-                  />
-                </View>
-                <View>
-                  <TextField
-                    label={i18n.t('label.Gift_Message')}
-                    value={props.values.message}
-                    tintColor={'#89b53a'}
-                    titleFontSize={12}
-                    returnKeyType="next"
-                    lineWidth={1}
-                    multiline
-                    labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    titleTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    blurOnSubmit={false}
-                    error={props.touched.message && props.errors.message}
-                    onChangeText={props.handleChange('message')}
-                    onBlur={props.handleBlur('message')}
-                  />
-                </View>
-              </KeyboardAwareScrollView>
+                </KeyboardAwareScrollView>
 
-              {this.state.buttonType === 'next' ? (
-                <>
+                {this.state.buttonType === 'next' ? (
+                  <>
+                    <TouchableOpacity
+                      style={[
+                        buttonStyles.dualActionButtonTouchable,
+                        { top: undefined, bottom: 55, flex: 1, padding: 20 }
+                      ]}
+                      onPress={props.handleSubmit}
+                    >
+                      <View
+                        style={[
+                          buttonStyles.dualActionButtonView2,
+                          !isValid
+                            ? { backgroundColor: 'rgba(137, 181, 58, 0.19)' }
+                            : {}
+                        ]}
+                      >
+                        <Text style={buttonStyles.dualActionButtonText2}>
+                          {i18n.t('label.continue_to_select_project')}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  </>
+                ) : null}
+
+                {this.state.buttonType === '>' ? (
                   <TouchableOpacity
                     style={[
-                      buttonStyles.dualActionButtonTouchable,
-                      { top: undefined, bottom: 55, flex: 1, padding: 20 }
+                      buttonStyles.actionButtonSmallTouchable,
+                      { top: undefined, bottom: '20%' },
+                      !props.isValid
+                        ? { backgroundColor: 'rgba(137, 181, 58, 0.19)' }
+                        : {}
                     ]}
                     onPress={props.handleSubmit}
                   >
-                    <View style={buttonStyles.dualActionButtonView2}>
-                      <Text style={buttonStyles.dualActionButtonText2}>
-                        {i18n.t('label.continue_to_select_project')}
-                      </Text>
-                    </View>
+                    <Image
+                      source={forward}
+                      resizeMode="cover"
+                      style={buttonStyles.actionButtonSmallImage}
+                    />
                   </TouchableOpacity>
-                </>
-              ) : null}
-
-              {this.state.buttonType === '>' ? (
-                <TouchableOpacity
-                  style={[
-                    buttonStyles.actionButtonSmallTouchable,
-                    { top: undefined, bottom: '20%' }
-                  ]}
-                  onPress={props.handleSubmit}
-                >
-                  <Image
-                    source={forward}
-                    resizeMode="cover"
-                    style={buttonStyles.actionButtonSmallImage}
-                  />
-                </TouchableOpacity>
-              ) : null}
-            </View>
-          </>
-        )}
+                ) : null}
+              </View>
+            </>
+          );
+        }}
       </Formik>
     );
   }

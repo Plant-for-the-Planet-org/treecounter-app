671f39260abb40deb00d390f2e91868428f8f725
haideralishah <headeralishah@gmail.com>
2019-12-20 15:12:45 +0500

    Leaderboard/countries - working UI : [on Progress]

--- index.native.js
+++ index.native.js
@@ -2,8 +2,9 @@ import { AppRegistry } from 'react-native';
 import App from './app/components/App';
 import './ReactotronConfig';
 /* app.js */
-
+import LeaderBoardRefresh from './app/components/LeaderboardRefresh/';
+import CountriesLeaderBoard from './app/components/LeaderboardRefresh/Countries';
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
 
-AppRegistry.registerComponent('TreecounterApp', () => App);
+AppRegistry.registerComponent('TreecounterApp', () => CountriesLeaderBoard);

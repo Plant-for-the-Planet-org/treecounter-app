ec09725522686c1e52a3d2025309f9e73945f940
mustak <mahmed0715@gmail.com>
2020-01-01 23:34:03 +0600

    Fixing currency issue with payment currency api https limitations issue

--- app/actions/fetchLocation.js
+++ app/actions/fetchLocation.js
@@ -1,5 +1,5 @@
 import { getItemSync } from '../stores/localStorage';
-import { getExternalRequest } from '../utils/api';
+import { getRequest } from '../utils/api';
 // Source: This is absolutely static data
 import countryCodes from '../assets/countryCodes.json';
 // Source: https://trilliontreecampaign.org/public/v1.1/en/currencies > rates
@@ -7,16 +7,11 @@ import countryCodes from '../assets/countryCodes.json';
 import supportedCurrency from '../assets/supportedCurrency.json';
 import { find } from 'lodash';
 import { setCurrencyAction } from './globalCurrency';
-import { context } from '../config';
 
 export function fetchLocation() {
   return dispatch => {
     if (!getItemSync('preferredCurrency')) {
-      getExternalRequest({
-        endPoint: `https://api.ipstack.com/check?access_key=${
-          context.locationApikKey
-        }&fields=location,country_code,currency`
-      })
+      getRequest('public_ipstack')
         .then(data => {
           // console.log('Got location fetch ip', data);
           const foundLocation = find(countryCodes, {

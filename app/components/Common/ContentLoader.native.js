2d5fdb99b86c4bf63c825d41badc02cca3160761
haideralishah <headeralishah@gmail.com>
2020-01-07 14:41:38 +0500

    Contetnt loader Desing Update

--- app/components/Common/ContentLoader.native.js
+++ app/components/Common/ContentLoader.native.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, Dimensions } from 'react-native';
 import ContentLoader from 'react-native-content-loader';
-import { Circle, Rect } from 'react-native-svg';
+import { Circle, Rect, Text } from 'react-native-svg';
 import i18n from '../../locales/i18n.js';
 import { loadingBar } from '../../assets';
 
+const HEIGHT = Dimensions.get('window').height;
+const WIDTH = Dimensions.get('window').width;
 const CompetitionLoader = () => (
   <ContentLoader
     height={550}
@@ -21,6 +23,23 @@ const CompetitionLoader = () => (
     <Rect x="100" y="460" rx="10" ry="10" width="140" height="20" />
     <Rect x="100" y="490" rx="10" ry="10" width="300" height="20" />
     <Rect x="100" y="520" rx="10" ry="10" width="250" height="20" />
+  </ContentLoader>
+);
+const SingleCompetitionLoader = () => (
+  <ContentLoader
+    height={550}
+    width={500}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Rect x="100" y="3" rx="10" ry="10" width="300" height="180" />
+    <Rect x="100" y="190" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="217" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="244" rx="10" ry="10" width="250" height="20" />
+    <Rect x="100" y="274" rx="10" ry="10" width="140" height="20" />
+    <Rect x="100" y="304" rx="10" ry="10" width="300" height="20" />
+    <Rect x="100" y="334" rx="10" ry="10" width="250" height="20" />
   </ContentLoader>
 );
 const LeaderBoardLoader = () => (
@@ -167,6 +186,21 @@ const DefaultLoader = () => (
   </View>
 );
 
+const PublicTreeCounterContentLoader = () => (
+  <ContentLoader
+    height={HEIGHT}
+    width={WIDTH}
+    speed={3}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <Circle cx="50" cy="60" r="30" />
+    <Rect x="100" y="40" rx="4" ry="4" width="220" height="13" />
+    <Rect x="100" y="70" rx="4" ry="4" width="100" height="13" />
+    <Circle cx="160" cy="270" r="106" />
+  </ContentLoader>
+);
+
 const ContentLoading = props => {
   const { screen } = props;
   return (
@@ -174,14 +208,17 @@ const ContentLoading = props => {
       {screen === 'AppHome' && <WorldLoader />}
       {screen === 'LeaderBoard' && <LeaderBoardLoader />}
       {screen === 'Competition' && <CompetitionLoader />}
-      {screen === 'worldLoader' && <DefaultLoader />}
+      {screen === 'SingleCompetition' && <SingleCompetitionLoader />}
+      {screen === 'PublicTreeCounterContentLoader' && (
+        <PublicTreeCounterContentLoader />
+      )}
       {screen === 'profileLoader' && <ProfileLoader />}
       {screen === 'publicProfileLoader' && <PublicProfileLoader />}
       {screen === 'competitionListLoader' && <CompetitionListLoader />}
       {screen === 'competitionSingleLoader' && <CompetitionSingleLoader />}
       {screen === 'projectListLoader' && <ProjectListLoader />}
+      {screen === 'worldLoader' && <WorldLoader />}
       {screen === 'projectSingleLoader' && <ProjectSingleLoader />}
-      {screen === 'defaultLoader' && <DefaultLoader />}
     </View>
   );
 };
@@ -196,3 +233,5 @@ const containerStyle = {
   justifyContent: 'center'
 };
 export default ContentLoading;
+
+export { PublicTreeCounterContentLoader };
--- index.native.js
+++ index.native.js
@@ -2,6 +2,7 @@ import { AppRegistry } from 'react-native';
 import App from './app/components/App';
 import './ReactotronConfig';
 /* app.js */
+import { PublicTreeCounterContentLoader } from './app/components/Common/ContentLoader.native';
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];

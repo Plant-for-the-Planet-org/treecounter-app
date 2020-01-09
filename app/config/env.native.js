1203320912afca67328e0cf47b07f134c03eb184
Norbert Schuler <norbert.schuler@epublica.de>
2020-01-09 15:30:42 +0100

    eslint fix

--- app/config/env.native.js
+++ app/config/env.native.js
@@ -1,4 +1,4 @@
 import Config from 'react-native-config';
 
 // copy Config to process.env to use React-Native like React
-for (e in Config) process.env[e] = Config[e];
+for (let e in Config) process.env[e] = Config[e];

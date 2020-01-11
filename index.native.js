d6b2cb9e8631161ffcf5e545d2bef324ce6badbb
haideralishah <headeralishah@gmail.com>
2020-01-11 14:37:17 +0500

    design Update

--- index.native.js
+++ index.native.js
@@ -2,7 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './app/components/App';
 import './ReactotronConfig';
 /* app.js */
-import { PublicTreeCounterContentLoader } from './app/components/Common/ContentLoader.native';
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];

b629d82c1c5ce06947a014363bc6175deff41560
Harsh Vitra <harshvitra@gmail.com>
2020-01-12 18:31:45 +0530

    Button Shows Above Keyboard

--- app/components/Authentication/Login/index.native.js
+++ app/components/Authentication/Login/index.native.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, Image, Keyboard, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView
+} from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import loginFormSchema from '../../../server/formSchemas/login';
 import i18n from '../../../locales/i18n.js';
@@ -56,7 +63,7 @@ export default class Login extends Component {
     const backgroundColor = 'white';
     const lockedButton = 'rgba(137, 181, 58, 0.19)';
     return (
-      <View style={{ flex: 1, paddingTop: 120 }}>
+      <View style={{ flex: 1 }}>
         <HeaderNew
           title={''}
           navigation={this.props.navigation}
@@ -85,21 +92,24 @@ export default class Login extends Component {
         >
           {props => (
             <>
-              <KeyboardAwareScrollView
-                contentContainerStyle={[
-                  styles.parentContainer,
-                  {
-                    backgroundColor: backgroundColor,
-                    padding: 20
-                  }
-                ]}
-                enableOnAndroid
-                keyboardDismissMode="on-drag"
-                keyboardShouldPersistTaps="always"
-                resetScrollToCoords={{ x: 0, y: 0 }}
-                scrollEnabled
-              >
-                {/* <View style={styles.headerContainer}>
+              <KeyboardAvoidingView behavior={'padding'} style={{ flex: 1 }}>
+                <KeyboardAwareScrollView
+                  contentContainerStyle={[
+                    styles.parentContainer,
+                    {
+                      backgroundColor: backgroundColor,
+                      padding: 20,
+                      paddingTop: 120,
+                      height: '100%'
+                    }
+                  ]}
+                  enableOnAndroid
+                  keyboardDismissMode="on-drag"
+                  keyboardShouldPersistTaps="always"
+                  resetScrollToCoords={{ x: 0, y: 0 }}
+                  scrollEnabled
+                >
+                  {/* <View style={styles.headerContainer}>
                   <Image
                     style={styles.imageStyle}
                     resizeMode={'contain'}
@@ -109,97 +119,100 @@ export default class Login extends Component {
                     {i18n.t('label.login_description')}
                   </Text>
                 </View> */}
-                <Text style={styles.loginTitle}>{i18n.t('label.log-in')}</Text>
-                <View>
-                  <TextField
-                    label={i18n.t('label.email')}
-                    // eslint-disable-next-line no-underscore-dangle
-                    value={props.values._username}
-                    tintColor={'#89b53a'}
-                    titleFontSize={12}
-                    lineWidth={1}
-                    // eslint-disable-next-line no-underscore-dangle
-                    error={props.touched._username && props.errors._username}
-                    labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    titleTextStyle={{ fontFamily: 'OpenSans-SemiBold' }}
-                    affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                    returnKeyType="next"
-                    onChangeText={props.handleChange('_username')}
-                    onBlur={props.handleBlur('_username')}
-                  />
-                </View>
 
-                <View style={styles.formView}>
-                  <View style={{ width: '100%' }}>
+                  <Text style={styles.loginTitle}>
+                    {i18n.t('label.log-in')}
+                  </Text>
+                  <View>
                     <TextField
-                      label={i18n.t('label.password')}
+                      label={i18n.t('label.email')}
                       // eslint-disable-next-line no-underscore-dangle
-                      value={props.values._password}
+                      value={props.values._username}
                       tintColor={'#89b53a'}
                       titleFontSize={12}
                       lineWidth={1}
-                      secureTextEntry={this.state.hidePassword}
+                      // eslint-disable-next-line no-underscore-dangle
+                      error={props.touched._username && props.errors._username}
                       labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
                       titleTextStyle={{ fontFamily: 'OpenSans-SemiBold' }}
                       affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
-                      blurOnSubmit={false}
-                      error={
-                        // eslint-disable-next-line no-underscore-dangle
-                        props.touched._password && props.errors._password
-                      }
-                      onChangeText={props.handleChange('_password')}
-                      onBlur={props.handleBlur('_password')}
+                      returnKeyType="next"
+                      onChangeText={props.handleChange('_username')}
+                      onBlur={props.handleBlur('_username')}
                     />
                   </View>
 
+                  <View style={styles.formView}>
+                    <View style={{ width: '100%' }}>
+                      <TextField
+                        label={i18n.t('label.password')}
+                        // eslint-disable-next-line no-underscore-dangle
+                        value={props.values._password}
+                        tintColor={'#89b53a'}
+                        titleFontSize={12}
+                        lineWidth={1}
+                        secureTextEntry={this.state.hidePassword}
+                        labelTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        titleTextStyle={{ fontFamily: 'OpenSans-SemiBold' }}
+                        affixTextStyle={{ fontFamily: 'OpenSans-Regular' }}
+                        blurOnSubmit={false}
+                        error={
+                          // eslint-disable-next-line no-underscore-dangle
+                          props.touched._password && props.errors._password
+                        }
+                        onChangeText={props.handleChange('_password')}
+                        onBlur={props.handleBlur('_password')}
+                      />
+                    </View>
+
+                    <TouchableOpacity
+                      onPress={() => this.togglePassword()}
+                      style={{ marginLeft: '-14%', bottom: -6 }}
+                    >
+                      {this.state.hidePassword ? (
+                        <Image
+                          source={closeeye}
+                          resizeMode={'contain'}
+                          style={{ height: 24 }}
+                        />
+                      ) : (
+                        <Image
+                          source={eye}
+                          resizeMode={'contain'}
+                          style={{ height: 24 }}
+                        />
+                      )}
+                    </TouchableOpacity>
+                  </View>
+
+                  <View style={[styles.bottomRow]}>
+                    <TouchableItem
+                      style={{ paddingRight: 5 }}
+                      onPress={this.onSignupClicked}
+                    >
+                      <Text style={styles.forgotPasswordHighlight}>
+                        {i18n.t('label.dont_have_account')}{' '}
+                        {i18n.t('label.signUp')}
+                      </Text>
+                    </TouchableItem>
+                  </View>
                   <TouchableOpacity
-                    onPress={() => this.togglePassword()}
-                    style={{ marginLeft: '-14%', bottom: -6 }}
+                    style={[styles.actionButtonTouchable]}
+                    onPress={props.isValid && props.handleSubmit}
                   >
-                    {this.state.hidePassword ? (
-                      <Image
-                        source={closeeye}
-                        resizeMode={'contain'}
-                        style={{ height: 24 }}
-                      />
-                    ) : (
-                      <Image
-                        source={eye}
-                        resizeMode={'contain'}
-                        style={{ height: 24 }}
-                      />
-                    )}
+                    <View
+                      style={[
+                        styles.actionButtonView,
+                        !props.isValid ? { backgroundColor: lockedButton } : {}
+                      ]}
+                    >
+                      <Text style={styles.actionButtonText}>
+                        {i18n.t('label.login')}
+                      </Text>
+                    </View>
                   </TouchableOpacity>
-                </View>
-
-                <View style={[styles.bottomRow]}>
-                  <TouchableItem
-                    style={{ paddingRight: 5 }}
-                    onPress={this.onSignupClicked}
-                  >
-                    <Text style={styles.forgotPasswordHighlight}>
-                      {i18n.t('label.dont_have_account')}{' '}
-                      {i18n.t('label.signUp')}
-                    </Text>
-                  </TouchableItem>
-                </View>
-              </KeyboardAwareScrollView>
-
-              <TouchableOpacity
-                style={[styles.actionButtonTouchable]}
-                onPress={props.isValid && props.handleSubmit}
-              >
-                <View
-                  style={[
-                    styles.actionButtonView,
-                    !props.isValid ? { backgroundColor: lockedButton } : {}
-                  ]}
-                >
-                  <Text style={styles.actionButtonText}>
-                    {i18n.t('label.login')}
-                  </Text>
-                </View>
-              </TouchableOpacity>
+                </KeyboardAwareScrollView>
+              </KeyboardAvoidingView>
             </>
           )}
         </Formik>

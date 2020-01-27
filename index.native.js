bdb561ab6e966c3aed4c140372bfb0df0eb905bd
haideralishah <headeralishah@gmail.com>
2020-01-27 19:25:36 +0500

    Bottom Scroll

--- index.native.js
+++ index.native.js
@@ -2,8 +2,9 @@ import { AppRegistry } from 'react-native';
 import App from './app/components/App';
 import './ReactotronConfig';
 /* app.js */
+import BottomDrawer from './TestingFile';
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
 
-AppRegistry.registerComponent('TreecounterApp', () => App);
+AppRegistry.registerComponent('TreecounterApp', () => BottomDrawer);

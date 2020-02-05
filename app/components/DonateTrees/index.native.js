b55ad5eb9a8f81d43923d9e5aaf8aedaed58ec2a
mustak <mahmed0715@gmail.com>
2020-02-05 17:08:05 +0600

    eslint

--- app/components/DonateTrees/index.native.js
+++ app/components/DonateTrees/index.native.js
@@ -14,7 +14,7 @@ import { Linking, SafeAreaView } from 'react-native';
 import { paymentFee } from '../../helpers/utils';
 import { getLocalRoute } from '../../actions/apiRouting';
 import { context } from '../../config';
-import TabContainer from '../../containers/Menu/TabContainer';
+// import TabContainer from '../../containers/Menu/TabContainer';
 import LoadingIndicator from '../Common/LoadingIndicator';
 
 export default class DonateTrees extends React.PureComponent {

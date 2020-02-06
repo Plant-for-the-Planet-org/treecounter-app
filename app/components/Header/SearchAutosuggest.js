19be070274a155bdfa0df1bba83772b232ef966e
Norbert Schuler <norbert.schuler@epublica.de>
2020-02-06 18:10:04 +0100

    eslint

--- app/components/Header/SearchAutosuggest.js
+++ app/components/Header/SearchAutosuggest.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 
 import { postDirectRequest } from '../../utils/api';
+import { treecounterLookupAction } from '../../actions/treecounterLookupAction';
 import {
   profile,
   country,
@@ -154,6 +155,10 @@ class SearchAutosuggest extends Component {
     );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ treecounterLookupAction }, dispatch);
+};
 
 SearchAutosuggest.propTypes = {
   onChangeName: PropTypes.func,

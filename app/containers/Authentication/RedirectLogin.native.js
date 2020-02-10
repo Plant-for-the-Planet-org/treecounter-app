bea88a6665666a91899bc5a742787522a6fd81d5
Harsh Vitra <harshvitra@gmail.com>
2020-02-10 23:34:14 +0530

    Show Login option instead of blank screen

--- app/containers/Authentication/RedirectLogin.native.js
+++ app/containers/Authentication/RedirectLogin.native.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity, Text, Image } from 'react-native';
 import { updateRoute } from '../../helpers/routerHelper';
-
+import { treelogo } from './../../assets';
 export default class RedirectLogin extends Component {
   componentDidMount() {
     updateRoute('app_login', this.props.navigation);
   }
   render() {
-    return <View />;
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Image source={treelogo} style={{ height: 120, width: 120 }} />
+          <Text style={{ marginTop: 24, fontFamily: 'OpenSans-Regular' }}>
+            Please login to visit this page
+          </Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#89ba43',
+              paddingHorizontal: 36,
+              marginTop: 24,
+              paddingVertical: 12,
+              borderRadius: 24
+            }}
+            onPress={() => updateRoute('app_login', this.props.navigation)}
+          >
+            <Text style={{ color: '#fff', fontFamily: 'OpenSans-SemiBold' }}>
+              Login
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }

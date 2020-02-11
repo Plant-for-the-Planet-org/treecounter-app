523f31e88f924765f8ceeb428ec752cbe9df451e
Norbert Schuler <norbert.schuler@epublica.de>
2020-02-11 16:04:36 +0100

    get bugsnagApiKey from environment

--- web/config/webpack.prod_server.config.js
+++ web/config/webpack.prod_server.config.js
@@ -11,69 +11,71 @@ const {
 } = require('webpack-bugsnag-plugins');
 const pkg = require('../../package.json');
 
-module.exports = webpackMerge(commonConfig, {
-  mode: 'production',
-  entry: {
-    bundle: [path.join(__dirname, '../../index.web.js')],
-    widget: [path.join(__dirname, '../widgets/basic/widget.js')],
-    treecounterwidget: [
-      path.join(__dirname, '../widgets/Treecounter/widget.js')
-    ],
-    donatetreewidget: [
-      path.join(__dirname, '../widgets/DonateTrees/widget.js')
-    ],
-    progressbarwidget: [
-      path.join(__dirname, '../widgets/progressbar/widget.js')
-    ]
-    // ndviwidget: [ path.join(__dirname, '../widgets/NDVI/widget.js') ]
-  },
-  output: {
-    path: path.join(__dirname, '../server'),
-    filename: '[name].[hash].js',
-    publicPath: '/'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['eslint-loader']
-      }
-    ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: false,
-        sourceMap: true, // Must be set to true if using source-maps in production
-        terserOptions: {
-          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-          keep_fnames: true
+module.exports = env => {
+  return webpackMerge(commonConfig, {
+    mode: 'production',
+    entry: {
+      bundle: [path.join(__dirname, '../../index.web.js')],
+      widget: [path.join(__dirname, '../widgets/basic/widget.js')],
+      treecounterwidget: [
+        path.join(__dirname, '../widgets/Treecounter/widget.js')
+      ],
+      donatetreewidget: [
+        path.join(__dirname, '../widgets/DonateTrees/widget.js')
+      ],
+      progressbarwidget: [
+        path.join(__dirname, '../widgets/progressbar/widget.js')
+      ]
+      // ndviwidget: [ path.join(__dirname, '../widgets/NDVI/widget.js') ]
+    },
+    output: {
+      path: path.join(__dirname, '../server'),
+      filename: '[name].[hash].js',
+      publicPath: '/'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['eslint-loader']
+        }
+      ]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: false,
+          sourceMap: true, // Must be set to true if using source-maps in production
+          terserOptions: {
+            // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+            keep_fnames: true
+          }
+        })
+      ]
+    },
+    plugins: [
+      new BugsnagBuildReporterPlugin({
+        apiKey: env.bugsnagApiKey,
+        appVersion: pkg.version
+      }),
+      new BugsnagSourceMapUploaderPlugin({
+        apiKey: env.bugsnagApiKey,
+        appVersion: pkg.version,
+        overwrite: true,
+        publicPath: '*'
+      }),
+      new WebpackCleanupPlugin(),
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          htmlLoader: {
+            minimize: false
+          }
         }
       })
     ]
-  },
-  plugins: [
-    new BugsnagBuildReporterPlugin({
-      apiKey: '6f2971a9b077662912f61ae602716afd',
-      appVersion: pkg.version
-    }),
-    new BugsnagSourceMapUploaderPlugin({
-      apiKey: '6f2971a9b077662912f61ae602716afd',
-      appVersion: pkg.version,
-      overwrite: true,
-      publicPath: '*'
-    }),
-    new WebpackCleanupPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        htmlLoader: {
-          minimize: false
-        }
-      }
-    })
-  ]
-});
+  });
+};

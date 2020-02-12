17b6df9034e4f1eab9a32373d618a09082cac01a
Harsh Vitra <harshvitra@gmail.com>
2020-02-13 00:16:15 +0530

    Resolution improved for Leaderboard

--- app/components/LeaderboardRefresh/Schools/SchoolsLeaderBoard.js
+++ app/components/LeaderboardRefresh/Schools/SchoolsLeaderBoard.js
@@ -74,7 +74,7 @@ const SchoolsLeaderBoard = ({ navigation }) => {
                     source={{
                       uri: getImageUrl(
                         'profile',
-                        'avatar',
+                        'thumb',
                         item.contributorAvatar
                       )
                     }}

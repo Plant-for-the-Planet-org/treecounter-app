638008f8a0212366fb565e5dabb31a6b8169d944
haideralishah <headeralishah@gmail.com>
2020-02-25 15:04:49 +0500

    List configure with FlatList

--- app/components/UserContributions/ContributionCardList.native.js
+++ app/components/UserContributions/ContributionCardList.native.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { FlatList } from 'react-native';
 import ContributionCard from './ContributionCard';
 
 const ContributionCardList = props => {
   const { contributions, deleteContribution, showAllContributions } = props;
 
-  return contributions.length > 3 && showAllContributions
-    ? contributions.map(
-        contribution =>
-          contribution && (
-            <ContributionCard
-              contribution={contribution}
-              deleteContribution={deleteContribution}
-              key={contribution.id}
-              navigation={props.navigation}
-            />
-          )
-      )
-    : contributions
-        .slice(0, 3)
-        .map(
-          contribution =>
-            contribution && (
-              <ContributionCard
-                contribution={contribution}
-                deleteContribution={deleteContribution}
-                key={contribution.id}
-                navigation={props.navigation}
-              />
-            )
-        );
+  return contributions.length > 3 && showAllContributions ? (
+    <FlatList
+      data={contributions}
+      renderItem={({ item }) => (
+        <ContributionCard
+          contribution={item}
+          deleteContribution={deleteContribution}
+          key={item.id}
+          navigation={props.navigation}
+        />
+      )}
+    />
+  ) : (
+    <FlatList
+      data={contributions.slice(0, 3)}
+      renderItem={({ item }) => (
+        <ContributionCard
+          contribution={item}
+          deleteContribution={deleteContribution}
+          key={item.id}
+          navigation={props.navigation}
+        />
+      )}
+    />
+  );
 };
 
 ContributionCardList.propTypes = {

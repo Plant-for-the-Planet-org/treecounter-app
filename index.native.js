cac1b950278fe8e623e169889c9a22c03ab7bcbb
haideralishah <headeralishah@gmail.com>
2020-02-27 12:37:45 +0500

    code refactor

--- index.native.js
+++ index.native.js
@@ -2,9 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './app/components/App';
 import './ReactotronConfig';
 /* app.js */
-import FullMapComponent, {
-  ListItem
-} from './app/components/UserHome/FullMapComponent';
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];

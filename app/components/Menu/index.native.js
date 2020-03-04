9db68e1b289edd6b0f0d87bf08af6c0daa52a1c9
Harsh Vitra <harshvitra@gmail.com>
2020-03-05 01:09:01 +0530

    ESLint

--- app/components/Menu/index.native.js
+++ app/components/Menu/index.native.js
@@ -12,7 +12,7 @@ import {
 } from '../../actions/apiRouting';
 import TouchableItem from '../../components/Common/TouchableItem.native';
 import UserProfileImage from '../Common/UserProfileImage.native';
-import GlobalCurrencySelector from '../Currency/GlobalCurrencySelector.native';
+// import GlobalCurrencySelector from '../Currency/GlobalCurrencySelector.native';
 import { LargeMenuItem } from './MenuItem.native';
 import countryCodes from '../../assets/countryCodes.json';
 import CurrencySelector from '../Common/CurrencySelectorList.native';

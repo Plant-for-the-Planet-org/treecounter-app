d0c1b72800640103a8c5da70cae7e994fe861678
haideralishah <headeralishah@gmail.com>
2020-03-09 15:52:53 +0500

    design update

--- index.native.js
+++ index.native.js
@@ -3,8 +3,6 @@ import App from './app/components/App';
 import './ReactotronConfig';
 /* app.js */
 
-// import App from './app/components/UserHome/MapExpandable'
-
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
 GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;

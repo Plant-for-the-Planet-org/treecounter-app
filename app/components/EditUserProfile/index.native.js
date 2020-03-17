a400446daa2c5986b5e8698735cd463bbc01d302
Harsh Vitra <harshvitra@gmail.com>
2020-03-17 22:52:06 +0530

    BG White in competitions and edit profile

--- app/components/EditUserProfile/index.native.js
+++ app/components/EditUserProfile/index.native.js
@@ -177,47 +177,47 @@ export default class EditUserProfile extends Component {
           <CardLayout style={{ flex: 1 }}>
             <ScrollView>
               {treeCounter &&
-              treeCounter.followeeIds &&
-              this.props.followeeList &&
-              this.props.followeeList.length > 0 ? (
-                <View>
-                  {this.props.followeeList.map(follow => (
-                    <View key={follow.id} style={styles.followerRow}>
-                      <UserProfileImage
-                        profileImage={follow.userProfile.image}
-                      />
-                      <TouchableItem
-                        style={styles.followerCol}
-                        onPress={() => {
-                          setTimeout(() => {
-                            this.props.navigation.navigate(
-                              getLocalRoute('app_treecounter'),
-                              {
-                                treeCounterId: follow.id,
-                                titleParam: follow.displayName
-                              }
-                            );
-                          }, 0);
-                        }}
-                      >
-                        <Text>{follow.displayName}</Text>
-                      </TouchableItem>
+                treeCounter.followeeIds &&
+                this.props.followeeList &&
+                this.props.followeeList.length > 0 ? (
+                  <View>
+                    {this.props.followeeList.map(follow => (
+                      <View key={follow.id} style={styles.followerRow}>
+                        <UserProfileImage
+                          profileImage={follow.userProfile.image}
+                        />
+                        <TouchableItem
+                          style={styles.followerCol}
+                          onPress={() => {
+                            setTimeout(() => {
+                              this.props.navigation.navigate(
+                                getLocalRoute('app_treecounter'),
+                                {
+                                  treeCounterId: follow.id,
+                                  titleParam: follow.displayName
+                                }
+                              );
+                            }, 0);
+                          }}
+                        >
+                          <Text>{follow.displayName}</Text>
+                        </TouchableItem>
 
-                      <FollowLabelButton
-                        label={i18n.t('label.unsubscribe')}
-                        isSubscribed
-                        onClick={() => {
-                          this.props.unfollowUser(follow.id);
-                        }}
-                      />
-                    </View>
-                  ))}
-                </View>
-              ) : this.props.followeeList ? (
-                <Text>{i18n.t('label.not_following_anybody')}</Text>
-              ) : (
-                <LoadingIndicator contentLoader screen="profileLoader" />
-              )}
+                        <FollowLabelButton
+                          label={i18n.t('label.unsubscribe')}
+                          isSubscribed
+                          onClick={() => {
+                            this.props.unfollowUser(follow.id);
+                          }}
+                        />
+                      </View>
+                    ))}
+                  </View>
+                ) : this.props.followeeList ? (
+                  <Text>{i18n.t('label.not_following_anybody')}</Text>
+                ) : (
+                    <LoadingIndicator contentLoader screen="profileLoader" />
+                  )}
             </ScrollView>
           </CardLayout>
         );
@@ -242,7 +242,7 @@ export default class EditUserProfile extends Component {
   render() {
     const textColor = '#4d5153';
     return (
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
         {/* <HeaderStatic
           title={i18n.t('label.edit_profile')}
           navigation={this.props.navigation}

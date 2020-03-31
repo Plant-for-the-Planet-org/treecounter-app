8f4a56f57084fde6e59bfd017258958c738715c0
haideralishah <headeralishah@gmail.com>
2020-03-31 18:42:34 +0500

    Icons set for detail page

--- app/components/UserHome/AnimatedMap.js
+++ app/components/UserHome/AnimatedMap.js
@@ -327,7 +327,7 @@ class AnimatedViews extends React.Component {
               <View
                 key={activeIndex}
               >
-                {markers && markers[activeIndex] ? <Animatable.View animation={activeIndex > lastActiveIndex ? 'bounceInRight' : 'bounceInLeft'} style={styles.card}>
+                {markers && markers[activeIndex] ? <Animatable.View duration={1500} animation={activeIndex > lastActiveIndex ? 'slideInRight' : 'slideInLeft'} style={styles.card}>
                   <ContributionCard
                     onPressSingleContribution={this.onPressMarker}
                     isFromAnimatredCardList
@@ -354,21 +354,19 @@ class AnimatedViews extends React.Component {
             >
               <Icon name={this.state.singleContributionID ? 'keyboard-arrow-left' : 'keyboard-arrow-down'} size={25} color={'#000'} />
             </TouchableOpacity>
-            {!this.state.singleContributionID ? <>
-              <TouchableOpacity
-                onPress={() => this.onPressHeader()}
-                style={styles.fullScreenExitIcon}
-              >
-                <Icon name={'fullscreen-exit'} size={30} color={'#4C5153'} />
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={this.onPressCurrentLocation}
-                style={styles.myLocationIcon}
-              >
-                <Icon name={'my-location'} size={30} color={'#4C5153'} />
-              </TouchableOpacity>
 
-            </> : null}
+            <TouchableOpacity
+              onPress={() => this.onPressHeader()}
+              style={[styles.fullScreenExitIcon, singleContributionID ? ({ bottom: 620, right: 150 }) : ({})]}
+            >
+              <Icon name={'fullscreen-exit'} size={30} color={'#4C5153'} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={this.onPressCurrentLocation}
+              style={[styles.myLocationIcon, singleContributionID ? ({ bottom: 620, right: 85 }) : ({})]}
+            >
+              <Icon name={'my-location'} size={30} color={'#4C5153'} />
+            </TouchableOpacity>
             <TouchableOpacity
               onPress={() => { this.setState({ isSatellite: !this.state.isSatellite }) }}
               style={[styles.satellite, { bottom: singleContributionID ? 620 : 310, }]}

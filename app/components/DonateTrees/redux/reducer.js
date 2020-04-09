16f887fe403e2d5b30d7b2e86e0ead2f861e23a1
ankitecd <agupta.ag19@gmail.com>
2020-04-10 00:14:13 +0530

    added actions in commented and hooks action for donate trees

--- app/components/DonateTrees/redux/reducer.js
+++ app/components/DonateTrees/redux/reducer.js
@@ -1,61 +1,81 @@
+import {
+  CLEAR_DONATION_REDUCER,
+  SET_CONTEXT,
+  SET_DONATION_DETAILS,
+  SET_DONOR_DETAILS,
+  SET_GIFT_CONTEXT_DETAILS,
+  SET_PAYMENT_DETAILS,
+  SET_PAYMENT_RESPONSE,
+  SET_PLEDGE_DETAILS,
+  SET_SUPPORT_DETAILS
+} from '../../../actions/types';
+
 const initialState = {
-  contextType: '', // 'gift', 'support', 'pledge', 'direct'
+  contextDetails: '', // 'gift', 'support', 'pledge', 'direct'
   giftDetails: [{}], // Array of Gift Details - firstname, lastname, email, message, treecount
   supportTreeCounterDetails: {}, // Supported Treecounter id
   pledgeDetails: {}, // Pledge Details
   donationDetails: {}, // Project details, selected Tree count, frequency
   donorDetails: {}, // Contact details of the Donor
   paymentResponse: {}, // Payment response from the server
-  paymentDetails: {} // Payment mode and the details 
+  paymentDetails: {} // Payment mode and the details
 };
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
-    case 'SET_CONTEXT':
+    case CLEAR_DONATION_REDUCER:
+      return initialState;
+
+    case SET_CONTEXT:
       return {
         ...state,
-        contextType: action.payload.contextType
+        contextDetails: action.payload.contextDetails
       };
-    case 'SET_GIFT_CONTEXT_DETAILS':
-      return {
-        ...state,
-        contextType: action.payload.contextType,
-        giftDetails: action.payload.giftDetails
-      };
-    case 'SET_SUPPORT_DETAILS':
-      return {
-        ...state,
-        contextType: action.payload.contextType,
-        supportTreeCounterDetails: action.payload.supportTreeCounterDetails
-      };
-    case 'SET_PLEDGE_DETAILS':
-      return {
-        ...state,
-        contextType: action.payload.contextType,
-        pledgeDetails: action.payload.pledgeDetails
-      };
-    case 'SET_DONATION_DETAILS':
+
+    case SET_DONATION_DETAILS:
       return {
         ...state,
         donationDetails: action.payload.donationDetails
       };
-    case 'SET_DONOR_DETAILS':
+
+    case SET_DONOR_DETAILS:
       return {
         ...state,
         donorDetails: action.payload.donorDetails
       };
-    case 'SET_PAYMENT_RESPONSE':
+
+    case SET_GIFT_CONTEXT_DETAILS:
       return {
         ...state,
-        paymentResponse: action.payload.paymentResponse
+        contextDetails: action.payload.contextDetails,
+        giftDetails: action.payload.giftDetails
       };
-    case 'SET_PAYMENT_DETAILS':
+
+    case SET_PAYMENT_DETAILS:
       return {
         ...state,
         paymentDetails: action.payload.paymentDetails
       };
-    case 'CLEAR_REDUCER':
-      return initialState;
+
+    case SET_PAYMENT_RESPONSE:
+      return {
+        ...state,
+        paymentResponse: action.payload.paymentResponse
+      };
+
+    case SET_PLEDGE_DETAILS:
+      return {
+        ...state,
+        contextDetails: action.payload.contextDetails,
+        pledgeDetails: action.payload.pledgeDetails
+      };
+
+    case SET_SUPPORT_DETAILS:
+      return {
+        ...state,
+        contextDetails: action.payload.contextDetails,
+        supportTreeCounterDetails: action.payload.supportTreeCounterDetails
+      };
     default:
       return state;
   }

040a6a79a08e82faa10167cd461b2b93f0413bdd
Norbert Schuler <norbert.schuler@epublica.de>
2020-05-01 17:55:05 +0700

    fixed user contribution links for web client

--- app/components/Common/Text/TextSpan.js
+++ app/components/Common/Text/TextSpan.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TransparentButton from '../Button/TransparentButton';
 
-const TextSpan = ({ children, strong }) => (
-  <div className={'pftp-text-span ' + (strong ? 'bold' : '')}>{children}</div>
+const TextSpan = ({ onPress, children, strong }) => (
+  <div className={'pftp-text-span ' + (strong ? 'bold' : '')}
+    onClick={() => onPress()}
+  >
+    {onPress ?
+      <TransparentButton onClick={onPress}>
+        {children}
+      </TransparentButton>
+      : children
+    }
+  </div>
 );
 
 TextSpan.propTypes = {
-  children: PropTypes.string,
-  strong: PropTypes.bool
+  children: PropTypes.node,
+  strong: PropTypes.bool,
+  onPress: PropTypes.func
 };
 
 export default TextSpan;

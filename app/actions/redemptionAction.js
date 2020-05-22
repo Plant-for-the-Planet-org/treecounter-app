ee5fbd48cce04182929c5cf1fd31209f669e81cf
Norbert Schuler <norbert.schuler@epublica.de>
2020-05-22 15:26:36 +0700

    Revert "Update redux store after redeem"
    This reverts commit c5e34eb0f04a93ae0677ed9cc00cf4c5197a8096.

--- app/actions/redemptionAction.js
+++ app/actions/redemptionAction.js
@@ -1,7 +1,5 @@
 import { postAuthenticatedRequest } from '../utils/api';
-import { treecounterSchema } from '../schemas/index';
-import { mergeEntities } from '../reducers/entitiesReducer';
-import { normalize } from 'normalizr';
+import { loadUserProfile } from './loadUserProfileAction';
 
 export function validateCodeAction(data, params) {
   return postAuthenticatedRequest('validateCode_post', data, params);
@@ -12,8 +10,9 @@ export function setRedemptionCodeAction(data, params) {
     return new Promise(function (resolve, reject) {
       postAuthenticatedRequest('convertCode_post', data, params)
         .then((res) => {
-          if (res.data.schemata && res.data.schemata.treecounter)
-            dispatch(mergeEntities(normalize(res.data.schemata.treecounter, treecounterSchema)));
+          dispatch(
+            loadUserProfile(null)
+          );
           resolve(res);
         }, (err) => {
           reject(err)

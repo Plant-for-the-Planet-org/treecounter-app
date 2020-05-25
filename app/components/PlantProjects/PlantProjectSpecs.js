fd4dd6082b831ddbbc486bfbdf55754883073977
sanjay radadiya <sanjay.radadiya91@gmail.com>
2020-05-25 10:48:02 +0530

    Disable Survival Rate on Web

--- app/components/PlantProjects/PlantProjectSpecs.js
+++ app/components/PlantProjects/PlantProjectSpecs.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import PlantProjectSpecsItem from './PlantProjectSpecsItem';
 import i18n from '../../locales/i18n.js';
 import { getISOToCountryName } from '../../helpers/utils';
-import { locationIcon, tree_survival } from '../../assets';
+import { locationIcon } from '../../assets';
 /**
  * see: https://github.com/Plant-for-the-Planet-org/treecounter-platform/wiki/Component-PlantProjectSpecs
  */
-const PlantProjectSpecs = ({ location, survivalRate, taxDeduction }) => {
+const PlantProjectSpecs = ({ location, taxDeduction }) => {
   const getTaxCountries = () => {
     return (
       taxDeduction &&
@@ -21,13 +21,6 @@ const PlantProjectSpecs = ({ location, survivalRate, taxDeduction }) => {
   return (
     <div className="project-specs-left__container">
       <PlantProjectSpecsItem icon={locationIcon} label={location} />
-
-      <PlantProjectSpecsItem
-        icon={tree_survival}
-        value={i18n.t('label.survival_rate')}
-        label={survivalRate + '%'}
-      />
-
       <div className="project-specs__taxdeductible">
         {taxDeduction && taxDeduction.length ? (
           <div>

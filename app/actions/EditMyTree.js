271c1803fde156daadc2ea6e76ba588890a92663
Norbert Schuler <norbert.schuler@epublica.de>
2020-06-22 19:27:56 +0700

    add double safety check

--- app/actions/EditMyTree.js
+++ app/actions/EditMyTree.js
@@ -57,7 +57,7 @@ export function editTree(plantContribution, plantId, navigation) {
       .catch(error => {
         debug(error);
         NotificationManager.error(
-          error.response.data ? error.response.data.message : i18n.t('label.error'),
+          error.response && error.response.data ? error.response.data.message : i18n.t('label.error'),
           i18n.t('label.error'),
           5000
         );

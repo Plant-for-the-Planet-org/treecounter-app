d7f4c6acdb5f216ff9e02a8014796035f5d61f9e
Norbert Schuler <norbert.schuler@epublica.de>
2020-08-29 10:52:39 +0200

    upgraded react-native to 0.63.2

--- index.native.js
+++ index.native.js
@@ -2,9 +2,9 @@ import { AppRegistry } from 'react-native';
 import 'react-native-get-random-values';
 import App from './app/components/App';
 import './ReactotronConfig';
-/* app.js */
+import { name as appName } from './app.json';
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
 GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
-AppRegistry.registerComponent('TreecounterApp', () => App);
+AppRegistry.registerComponent(appName, () => App);
---
+++ ios/Dummy.swift
@@ -0,0 +1,11 @@
+//
+//  Dummy.swift
+//  TreecounterApp
+//
+//  Created by Norbert Schuler on 29.08.20.
+//  Copyright © 2020 Plant-for-the-Planet. All rights reserved.
+//
+
+// Dummy file to solve: https://github.com/react-native-community/upgrade-support/issues/25
+
+import Foundation

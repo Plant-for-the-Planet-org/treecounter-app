c95e42f069c72522d25d966f31e3cceddf58d99c
Norbert Schuler <norbert.schuler@epublica.de>
2020-10-01 11:50:50 +0200

    added tipsi-stripe manually again

--- android/app/src/main/java/org/pftp/MainApplication.java
+++ android/app/src/main/java/org/pftp/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 
 import java.lang.reflect.InvocationTargetException;
+
+import com.gettipsi.stripe.StripeReactPackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -28,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
             List<ReactPackage> packages = new PackageList(this).getPackages();
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(new MyReactNativePackage());
+            packages.add(new StripeReactPackage());
             return packages;
         }
 

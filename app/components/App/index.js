37144c5bcaab896622b99b1478db2223eab70cd9
Norbert Schuler <norbert.schuler@epublica.de>
2020-10-18 16:00:20 +0200

    added browser check

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -5,6 +5,7 @@ import TreeCounter from './TreeCounter';
 import configureStore from '../../stores/TreecounterStore';
 import GlobalErrorBoundary from '../ErrorBoundry/globalErrorBoundry';
 import { isIOS, isAndroid } from '../../utils/utils';
+import { browserNotCompatible } from '../../utils/browercheck.js';
 import SmartBannerClickable from '../SmartBanner';
 import i18n from '../../locales/i18n.js';
 let store;
@@ -16,21 +17,30 @@ export default class App extends Component {
   }
 
   render() {
-    return (
-      <Provider store={store}>
-        <GlobalErrorBoundary>
-          <TreeCounter />
-          <SmartBannerClickable
-            daysHidden={1}
-            title={i18n.t('label.plant_for_the_planet_app')}
-            author={i18n.t('label.a_solution_for_climate_crisis')}
-            button={i18n.t('label.get_app_button_text')}
-            force={isIOS() ? 'ios' : isAndroid() ? 'android' : ''}
-            position={'bottom'}
-          />
-        </GlobalErrorBoundary>
-      </Provider>
-    );
+    if (browserNotCompatible()) {
+      return (
+        <div className="app">
+          Browser is not compatible. Please download a newer version.
+        </div>
+      );
+    }
+    else {
+      return (
+        <Provider store={store}>
+          <GlobalErrorBoundary>
+            <TreeCounter />
+            <SmartBannerClickable
+              daysHidden={1}
+              title={i18n.t('label.plant_for_the_planet_app')}
+              author={i18n.t('label.a_solution_for_climate_crisis')}
+              button={i18n.t('label.get_app_button_text')}
+              force={isIOS() ? 'ios' : isAndroid() ? 'android' : ''}
+              position={'bottom'}
+            />
+          </GlobalErrorBoundary>
+        </Provider>
+      );
+    }
   }
 }
 
---
+++ app/utils/browercheck.js
@@ -0,0 +1,33 @@
+const userAgent = (navigator && navigator.userAgent || '').toLowerCase();
+
+// build a 'comparator' object for various comparison checks
+const comparator = {
+    '<': function(a, b) { return a < b; },
+    '<=': function(a, b) { return a <= b; },
+    '>': function(a, b) { return a > b; },
+    '>=': function(a, b) { return a >= b; }
+};
+
+// helper function which compares a version to a range
+function compareVersion(version, range) {
+    let string = (range + '');
+    let n = +(string.match(/\d+/) || NaN);
+    let op = string.match(/^[<>]=?|/)[0];
+    return comparator[op] ? comparator[op](version, n) : (version == n || n !== n);
+}
+
+// check for safari version
+function safari(range) {
+  let match = userAgent.match(/version\/(\d+).+?safari/);
+  return match !== null && compareVersion(match[1], range);
+}
+
+// check for samsung version
+function samsung(range) {
+  let match = userAgent.match(/samsungbrowser\/(\d+)/);
+  return match !== null && compareVersion(match[1], range);
+}
+
+export function browserNotCompatible() {
+  return safari('<10') || samsung('<6') || !Object.values || !window.Intl;
+}

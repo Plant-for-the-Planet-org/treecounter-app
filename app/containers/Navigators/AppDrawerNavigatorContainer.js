0ee50c91be13ac71ad5f581ff4560746c2ca1a9b
Norbert Schuler <norbert.schuler@epublica.de>
2021-01-15 15:33:38 +0100

    check for Auth0 or JWT token to load profile

--- app/containers/Navigators/AppDrawerNavigatorContainer.js
+++ app/containers/Navigators/AppDrawerNavigatorContainer.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { getAppNavigator } from '../../components/Navigators/AppDrawerNavigator';
-import { getAccessToken } from '../../utils/user';
+import { getAccessToken, getAuth0AccessToken } from '../../utils/user';
 import { loadUserProfile } from '../../actions/loadUserProfileAction';
 import { currentUserProfileSelector } from '../../selectors';
 import LoadingIndicator from '../../components/Common/LoadingIndicator';
@@ -71,8 +71,9 @@ class AppDrawerNavigatorContainer extends Component {
     if (isLoggedIn) {
       this.setState({ loading: false, isLoggedIn: true });
     } else {
+      let auth0Token = await getAuth0AccessToken();
       let token = await getAccessToken();
-      if (token) {
+      if (auth0Token || token) {
         this.props.loadUserProfile().catch(() => {
           this.setState({ loading: false, isLoggedIn: false });
         });

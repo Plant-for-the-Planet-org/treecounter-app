931fcf308ee135ccdbfbcc2dc66e26ac64b55310
Norbert Schuler <norbert.schuler@epublica.de>
2022-05-28 17:47:39 +0200

    move EStyleSheet to top level file

--- index.native.js
+++ index.native.js
@@ -1,8 +1,31 @@
 import { AppRegistry } from 'react-native';
 import 'react-native-get-random-values';
 import App from './app/components/App';
+import EStyleSheet from 'react-native-extended-stylesheet';
 import './ReactotronConfig';
 import { name as appName } from './app.json';
+
+EStyleSheet.build({
+    // always call EStyleSheet.build() even if you don't use global variables!
+    $primary: '#b9d384',
+    $textColor: '#686060',
+    $placeholderColor: '#e9e9e9',
+    $colorPrimary: '#b7d37f',
+    $colorPrimaryDark: '#b7d37f',
+    $colorPrimaryAccent: '#e86f56',
+    $colorPrimaryAccentLight: '#ec6453',
+    $borderColor: '#aba2a2',
+    $inputBorderColor: '#dad7d7',
+    $backgroundScreen: '#f1f1f1',
+    $colorError: '#ff0033',
+    $colorRedeemBorder: '#9fc356',
+    $colorRedeemInside: '#f5fbe8',
+    $cardTextColor: '#686060',
+    $lightTextColor: '#9c9b9b',
+
+    $newPrimary: '#89b53a',
+    $greyColor: '#d3d3d3'
+  });
 
 console.disableYellowBox = true;
 console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];

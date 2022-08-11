aabb0cc9a7ecad79754f89974e91644587887902
Norbert Schuler <norbert.schuler@epublica.de>
2022-08-11 15:15:21 +0700

    fixed missing giverName or giver for gifts in old api call

--- app/components/UserContributions/ContributionCard.native.js
+++ app/components/UserContributions/ContributionCard.native.js
@@ -444,7 +444,9 @@ class ContributionCard extends React.Component {
               <View style={styles.redeemObjectTreesContainer}>
                 <View style={styles.row1}>
                   <Text style={styles.redeemObjectTitle}>
-                    {i18n.t('label.gifted_from_person') + (contribution.giverName || contribution.giver)}
+                    {(contribution.giverName || contribution.giver) ?
+                      i18n.t('label.gifted_from_person') + (contribution.giverName || contribution.giver) :
+                      i18n.t('label.gift')}
                   </Text>
                   <View style={styles.row2}>
                     <Text style={styles.redeemObjectSubTitle}>{tpoLine}</Text>
